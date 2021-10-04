d3a06ba8e73975af092f3a063b829c77a73b689e
Yifei Sun <ysun@hey.com>
2021-10-04 16:53:06 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 git clone git@github.com:Yifei-S/Dotfiles.git ~/.dotfiles/
 
 ln -fsv ~/.dotfiles/zsh/zshrc ~/.zshrc
-ln -fsv ~/.dotfiles/vim/vimrc ~/.nvimrc
+ln -fsv ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -fsv ~/.dotfiles/git/gitconfig ~/.gitconfig
