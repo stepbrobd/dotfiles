f30401c60bb1c8e93eecff493a7a9ab3727222db
StepBroBD <Hi@StepBroBD.com>
2022-01-06 08:51:14 -0700

    neovim

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
-git clone https://github.com/StepBroBD/Dotfiles.git                ~/.dotfiles/
-git clone https://github.com/zsh-users/zsh-autosuggestions.git     ~/.dotfiles/zsh/zsh-autosuggestions/
+git clone https://github.com/StepBroBD/Dotfiles.git ~/.dotfiles/
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.dotfiles/zsh/zsh-autosuggestions/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.dotfiles/zsh/zsh-syntax-highlighting/
 
-ln -fsv ~/.dotfiles/zsh/zshrc      ~/.zshrc
-ln -fsv ~/.dotfiles/vim/vimrc      ~/.vimrc
-ln -fsv ~/.dotfiles/git/gitconfig  ~/.gitconfig
+mkdir ~/.config/nvim
+ln -fsv ~/.dotfiles/nvim ~/.config/nvim
+ln -fsv ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -fsv ~/.dotfiles/git/gitconfig ~/.gitconfig
