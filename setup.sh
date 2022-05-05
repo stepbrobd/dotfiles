23577fb6f1f400c193f768090e14f14e5ffb0519
StepBroBD <Hi@StepBroBD.com>
2022-05-05 12:46:39 -0600

    git global gpg config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.dotfiles/zsh/z
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.dotfiles/zsh/zsh-syntax-highlighting/
 
 ln -fsv ~/.dotfiles/zsh/zshrc ~/.zshrc
-ln -fsv ~/.dotfiles/gpg/gpg.sh ~/.gpg.sh
+ln -fsv ~/.dotfiles/gpg/gpg.conf ~/.gnupg/gpg.conf
 ln -fsv ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -fsv ~/.dotfiles/nvim ~/.config
