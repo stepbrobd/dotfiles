f4557c797cbde536cea40a29cb5c58f0170ef3fe
StepBroBD <Hi@StepBroBD.com>
2022-05-17 13:22:11 -0600

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
-#!/bin/zsh
+#!/bin/sh
 
 git clone https://github.com/StepBroBD/Dotfiles.git ~/.dotfiles/
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.dotfiles/zsh/zsh-autosuggestions/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.dotfiles/zsh/zsh-syntax-highlighting/
 
+mkdir ~/.config
+mkdir ~/.gnupg
+
 ln -fsv ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -fsv ~/.dotfiles/gpg/gpg.conf ~/.gnupg/gpg.conf
 ln -fsv ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -fsv ~/.dotfiles/nvim ~/.config
+
+chown -R "$(whoami)" ~/.gnupg/
+chmod 600 ~/.gnupg/*
+chmod 700 ~/.gnupg
+
+/bin/zsh
+sourec ~/.zshrc
