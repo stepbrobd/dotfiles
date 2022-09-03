2c6756410b449c03ab6e8e4d68cb69327112c7df
StepBroBD <Hi@StepBroBD.com>
2022-09-03 14:04:20 -0600

    config update

--- setup.sh
+++ setup.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-if [[ -f ~/.dotfiles/ ]]; then
-  rm -rf ~/.dotfiles/
+if [[ -f ~/.config/dotfiles/ ]]; then
+  rm -rf ~/.config/dotfiles/
 fi
 
-git clone https://github.com/StepBroBD/Dotfiles.git ~/.dotfiles/
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.dotfiles/zsh/zsh-autosuggestions/
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.dotfiles/zsh/zsh-syntax-highlighting/
+git clone https://github.com/StepBroBD/Dotfiles.git                ~/.config/dotfiles/
+git clone https://github.com/zsh-users/zsh-autosuggestions.git     ~/.config/dotfiles/zsh/zsh-autosuggestions/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/dotfiles/zsh/zsh-syntax-highlighting/
 
 mkdir ~/.config
 mkdir ~/.gnupg
 
-ln -fsv ~/.dotfiles/zsh/zshrc ~/.zshrc
-ln -fsv ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -fsv ~/.dotfiles/gpg/gpg.conf ~/.gnupg/gpg.conf
-ln -fsv ~/.dotfiles/git/gitconfig ~/.gitconfig
-ln -fsv ~/.dotfiles/nvim ~/.config
+ln -fsv ~/.config/dotfiles/zsh/zshrc          ~/.zshrc
+ln -fsv ~/.config/dotfiles/tmux/tmux.conf     ~/.tmux.conf
+ln -fsv ~/.config/dotfiles/gpg/gpg.conf       ~/.gnupg/gpg.conf
+ln -fsv ~/.config/dotfiles/gpg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+ln -fsv ~/.config/dotfiles/git/gitconfig      ~/.gitconfig
+ln -fsv ~/.config/dotfiles/nvim               ~/.config
 
 chown -R "$(whoami)" ~/.gnupg/
-chmod 600 ~/.gnupg/*
-chmod 700 ~/.gnupg
+chmod 600            ~/.gnupg/*
+chmod 700            ~/.gnupg
 
 sudo chsh -s /bin/zsh
