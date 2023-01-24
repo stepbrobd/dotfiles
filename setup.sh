3a0258dd07269cb6998661c4928da98bd884effe
StepBroBD <Hi@StepBroBD.com>
2023-01-23 17:04:46 -0700

    fix tmux plugin setup

--- setup.sh
+++ setup.sh
@@ -7,13 +7,14 @@ fi
 git clone https://github.com/StepBroBD/Dotfiles.git                ~/.config/dotfiles/
 git clone https://github.com/zsh-users/zsh-autosuggestions.git     ~/.config/dotfiles/zsh/zsh-autosuggestions/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/dotfiles/zsh/zsh-syntax-highlighting/
-git clone https://github.com/tmux-plugins/tpm                      ~/.config/dotfiles/tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm                      ~/.tmux/plugins/tpm
 
 mkdir ~/.config
 mkdir ~/.gnupg
 
 ln -fsv ~/.config/dotfiles/zsh/zshrc          ~/.zshrc
 ln -fsv ~/.config/dotfiles/tmux/tmux.conf     ~/.tmux.conf
+ln -fsv ~/.tmux/plugins/                      ~/.config/dotfiles/tmux/plugins
 ln -fsv ~/.config/dotfiles/gpg/gpg.conf       ~/.gnupg/gpg.conf
 ln -fsv ~/.config/dotfiles/gpg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 ln -fsv ~/.config/dotfiles/nvim               ~/.config
