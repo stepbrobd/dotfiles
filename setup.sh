2e22bdcf32994cf468339306cf16458484361ec8
StepBroBD <Hi@StepBroBD.com>
2023-02-05 20:50:19 -0700

    nix rebuild alias

--- setup.sh
+++ setup.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-if [[ -f ~/.config/dotfiles/ ]]; then
-  rm -rf ~/.config/dotfiles/
+if [[ -f "${HOME}/.config/dotfiles/" ]]; then
+  rm -rf "${HOME}/.config/dotfiles/"
 fi
 
-git clone https://github.com/StepBroBD/Dotfiles.git ~/.config/dotfiles/
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/dotfiles/zsh/zsh-autosuggestions/
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/dotfiles/zsh/zsh-syntax-highlighting/
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/StepBroBD/Dotfiles.git "${HOME}/.config/dotfiles/"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "${HOME}/.config/dotfiles/zsh/zsh-autosuggestions/"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${HOME}/.config/dotfiles/zsh/zsh-syntax-highlighting/"
+git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
 
-mkdir ~/.config
-mkdir ~/.gnupg
+mkdir "${HOME}/.config"
+mkdir "${HOME}/.gnupg"
 
-ln -fsv ~/.config/dotfiles/zsh/zshrc ~/.zshrc
-ln -fsv ~/.config/dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -fsv ~/.tmux/plugins/ ~/.config/dotfiles/tmux/plugins
-ln -fsv ~/.config/dotfiles/gpg/gpg.conf ~/.gnupg/gpg.conf
-ln -fsv ~/.config/dotfiles/gpg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
-ln -fsv ~/.config/dotfiles/nvim ~/.config
+ln -fsv "${HOME}/.config/dotfiles/zsh/zshrc ~/.zshrc"
+ln -fsv "${HOME}/.config/dotfiles/tmux/tmux.conf ~/.tmux.conf"
+ln -fsv "${HOME}/.tmux/plugins/ ~/.config/dotfiles/tmux/plugins"
+ln -fsv "${HOME}/.config/dotfiles/gpg/gpg.conf ~/.gnupg/gpg.conf"
+ln -fsv "${HOME}/.config/dotfiles/gpg/gpg-agent.conf ~/.gnupg/gpg-agent.conf"
+ln -fsv "${HOME}/.config/dotfiles/nvim ~/.config"
 
 if [[ $(command -v smimesign) > /dev/null ]]; then
-  ln -fsv ~/.config/dotfiles/git/smime.gitconfig ~/.gitconfig
+  ln -fsv "${HOME}/.config/dotfiles/git/smime.gitconfig ~/.gitconfig"
 else
-  ln -fsv ~/.config/dotfiles/git/gpg.gitconfig ~/.gitconfig
+  ln -fsv "${HOME}/.config/dotfiles/git/gpg.gitconfig ~/.gitconfig"
 fi
 
-chown -R "$(whoami)" ~/.gnupg/
-chmod 600 ~/.gnupg/*
-chmod 700 ~/.gnupg
+chown -R "$(whoami)" "${HOME}/.gnupg/"
+chmod 600 "${HOME}/.gnupg/*"
+chmod 700 "${HOME}/.gnupg"
+
+chmod +x "${HOME}/.config/dotfiles/nix/darwin-rebuild.sh"
+chmod +x "${HOME}/.config/dotfiles/nix/nixos-rebuild.sh"
 
 chsh -s /bin/zsh
