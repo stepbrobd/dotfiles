7794b58ce1ca09b70b0339932be0a593a20df8d1
Yifei Sun <ysun@hey.com>
2023-02-08 23:52:27 -0700

    add zsh prompt

--- setup.sh
+++ setup.sh
@@ -5,25 +5,27 @@ if [[ -f "${HOME}/.config/dotfiles/" ]]; then
 fi
 
 git clone https://github.com/StepBroBD/Dotfiles.git "${HOME}/.config/dotfiles/"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "${HOME}/.config/dotfiles/zsh/zsh-autosuggestions/"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${HOME}/.config/dotfiles/zsh/zsh-syntax-highlighting/"
-git clone https://github.com/chisui/zsh-nix-shell.git "${HOME}/.config/dotfiles/zsh/zsh-nix-shell/"
-git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${HOME}/.config/dotfiles/zsh/powerlevel10k/"
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git "${HOME}/.config/dotfiles/zsh/zsh-autosuggestions/"
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${HOME}/.config/dotfiles/zsh/zsh-syntax-highlighting/"
+git clone --depth=1 https://github.com/chisui/zsh-nix-shell.git "${HOME}/.config/dotfiles/zsh/zsh-nix-shell/"
+git clone --depth=1 https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
 
 mkdir "${HOME}/.config"
 mkdir "${HOME}/.gnupg"
 
-ln -fsv "${HOME}/.config/dotfiles/zsh/zshrc ~/.zshrc"
-ln -fsv "${HOME}/.config/dotfiles/tmux/tmux.conf ~/.tmux.conf"
-ln -fsv "${HOME}/.tmux/plugins/ ~/.config/dotfiles/tmux/plugins"
-ln -fsv "${HOME}/.config/dotfiles/gpg/gpg.conf ~/.gnupg/gpg.conf"
-ln -fsv "${HOME}/.config/dotfiles/gpg/gpg-agent.conf ~/.gnupg/gpg-agent.conf"
-ln -fsv "${HOME}/.config/dotfiles/nvim ~/.config"
+ln -fsv "${HOME}/.config/dotfiles/zsh/zshrc" "${HOME}/.zshrc"
+ln -fsv "${HOME}/.config/dotfiles/zsh/p10k" "${HOME}/.p10k.zsh"
+ln -fsv "${HOME}/.config/dotfiles/tmux/tmux.conf" "${HOME}/.tmux.conf"
+ln -fsv "${HOME}/.tmux/plugins/" "${HOME}/.config/dotfiles/tmux/plugins"
+ln -fsv "${HOME}/.config/dotfiles/gpg/gpg.conf" "${HOME}/.gnupg/gpg.conf"
+ln -fsv "${HOME}/.config/dotfiles/gpg/gpg-agent.conf" "${HOME}/.gnupg/gpg-agent.conf"
+ln -fsv "${HOME}/.config/dotfiles/nvim" "${HOME}/.config"
 
 if [[ $(command -v smimesign) > /dev/null ]]; then
-  ln -fsv "${HOME}/.config/dotfiles/git/smime.gitconfig ~/.gitconfig"
+  ln -fsv "${HOME}/.config/dotfiles/git/smime.gitconfig" "${HOME}/.gitconfig"
 else
-  ln -fsv "${HOME}/.config/dotfiles/git/gpg.gitconfig ~/.gitconfig"
+  ln -fsv "${HOME}/.config/dotfiles/git/gpg.gitconfig" "${HOME}/.gitconfig"
 fi
 
 chown -R "$(whoami)" "${HOME}/.gnupg/"
@@ -34,3 +36,4 @@ chmod +x "${HOME}/.config/dotfiles/nix/darwin-rebuild.sh"
 chmod +x "${HOME}/.config/dotfiles/nix/nixos-rebuild.sh"
 
 chsh -s /bin/zsh
+
