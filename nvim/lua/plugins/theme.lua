9c6532876a89dc77a32b57d635120fa86f344d4c
StepBroBD <Hi@StepBroBD.com>
2023-02-25 21:16:51 -0700

    change old nvim config to lazy.nvim

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"projekt0n/github-nvim-theme",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("github-theme").setup({
+				theme_style = "dimmed",
+			})
+		end,
+	},
+
+	{
+		"LazyVim/LazyVim",
+		opts = { colorscheme = "github_dimmed" },
+	},
+}
