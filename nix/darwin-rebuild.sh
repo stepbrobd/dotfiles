a9b1d6bff7989f247e1cc9faeee031c678bd17a6
Yifei Sun <ysun@hey.com>
2023-02-27 01:05:50 -0700

    updated darwin-rebuild

--- nix/darwin-rebuild.sh
+++ nix/darwin-rebuild.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [[ $(uname) == "Darwin" ]]; then
+  printf "\e[36;1mThis operation requires \`sudo\`\e[0m\n"
+  sudo echo -n
+
   pushd $TMPDIR >/dev/null
 
   if [ -e ./flake-aarch-64-darwin-loader/flake.nix ]; then
@@ -8,7 +11,11 @@ if [[ $(uname) == "Darwin" ]]; then
   fi
 
   (yes "" | sh <(curl -L https://mynixos.com/install-loader) flake/aarch64-darwin) >/dev/null 2>&1
+
   (nix flake lock ./flake-aarch-64-darwin-loader) >/dev/null 2>&1
+
+  sudo -i nix-channel --update
+
   darwin-rebuild switch --flake ./flake-aarch-64-darwin-loader\#aarch64
 
   popd >/dev/null
