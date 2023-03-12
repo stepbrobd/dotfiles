1b3ffdf98798c328375b750651d61cfb3bde2ea1
StepBroBD <Hi@StepBroBD.com>
2023-03-11 21:21:10 -0700

    rebuild command changes

--- scripts/nixos-rebuild.sh
+++ scripts/nixos-rebuild.sh
@@ -5,19 +5,19 @@ attr=$([ $(uname -m) = "x86_64" ] && echo "x86_64" || echo "aarch64")
 printf "\e[36;1mThis operation requires \`sudo\`\e[0m\n"
 sudo echo -n
 
-pushd $TMPDIR >/dev/null
+pushd /tmp >/dev/null
 
 if [ -e ./stepbrobd-dotfiles-loader/flake.nix ]; then
-	rm -rf ./stepbrobd-dotfiles-loader
+    rm -rf ./stepbrobd-dotfiles-loader
 fi
 
 sudo -i nix-channel --update
 (yes "" | sh <(curl -L https://mynixos.com/install-loader) stepbrobd/dotfiles) >/dev/null 2>&1
 (nix flake lock ./stepbrobd-dotfiles-loader) >/dev/null 2>&1
-darwin-rebuild switch --flake ./stepbrobd-dotfiles-loader\#$attr --show-trace
+nixos-rebuild switch --flake ./stepbrobd-dotfiles-loader\#$attr --show-trace
 
 if [ -e ./stepbrobd-dotfiles-loader/flake.nix ]; then
-	rm -rf ./stepbrobd-dotfiles-loader
+    rm -rf ./stepbrobd-dotfiles-loader
 fi
 
 popd >/dev/null
