7218ec8863b218999eb62f4cd6b01f8002fefaf5
StepBroBD <Hi@StepBroBD.com>
2023-04-08 15:41:39 -0600

    repos init

--- scripts/repos.sh
+++ scripts/repos.sh
@@ -3,17 +3,7 @@
 set -eo pipefail
 
 if [[ $1 == "help" ]] || [[ $# != 1 ]]; then
-    echo "Usage: repos [reload|update]"
+    echo "Usage: repos [add|ls|rm]"
 fi
 
-if [[ $1 == "reload" ]]; then
-    echo 'Reloading \e[4m~/.zshrc\e[0m...'
-    source ~/.zshrc
-fi
 
-if [[ $1 == "update" ]]; then
-    echo 'Updating \e[4m~/.config/dotfiles\e[0m...'
-    pushd ~/.config/dotfiles >/dev/null
-    git pull
-    popd >/dev/null
-fi!/bin/bash
