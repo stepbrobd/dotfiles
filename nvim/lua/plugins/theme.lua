6126c72cfb0479abc878e629b44cf8ca53d041d4
StepBroBD <Hi@StepBroBD.com>
2023-04-23 20:31:58 -0600

    nvim lazy update

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -5,11 +5,11 @@ return {
 		priority = 1000,
 		config = function()
 			require("github-theme").setup({})
-			vim.cmd("colorscheme github_dimmed")
+			vim.cmd("colorscheme github_dark_dimmed")
 		end,
 	},
 	{
 		"LazyVim/LazyVim",
-		opts = { colorscheme = "github_dimmed" },
+		opts = { colorscheme = "github_dark_dimmed" },
 	},
 }
