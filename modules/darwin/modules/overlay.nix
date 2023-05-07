3fc5653964f6038d55fe913a79799a87ff6fc7d5
Yifei Sun <ysun@hey.com>
2023-05-07 14:15:55 -0600

    add neovim-overlay

--- modules/darwin/modules/overlay.nix
+++ modules/darwin/modules/overlay.nix
@@ -2,6 +2,7 @@
 { config, lib, pkgs, ... }: {
   config.nixpkgs.overlays = [
     (self: super: {
+      neovim = inputs.neovim-overlay.packages.${super.system}.neovim;
       vscode = inputs.vscode-overlay.packages.${super.system}.vscodeInsiders;
       raycast = inputs.raycast-overlay.packages.${super.system}.raycast;
     })
