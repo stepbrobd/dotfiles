deda63ef375e252c21c84295b76ca0f58eec88e8
StepBroBD <Hi@StepBroBD.com>
2023-05-07 12:12:56 -0600

    refactor: pkgs

---
+++ modules/homeModules/pkgs.nix
@@ -0,0 +1,62 @@
+{ inputs, ... }@context:
+{ config, lib, pkgs, ... }: {
+  config.home.packages = with pkgs; with pkgs.nodePackages_latest; [
+    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+
+    cacert
+    nix
+    nixpkgs-fmt
+    cachix
+    direnv
+    coreutils
+    llvm
+    rustup
+    go
+    typst
+    texlive.combined.scheme-full
+    nodejs
+    pnpm
+    vercel
+    "@githubnext/github-copilot-cli"
+    tmux
+    bat
+    exa
+    btop
+    curl
+    wget
+    ripgrep
+    git
+    gh
+    jq
+    mcfly
+    gnupg
+    neovim
+    lazygit
+    fzf
+    fd
+    tree-sitter
+    entr
+    code-minimap
+    qemu
+    docker
+    terraform
+    awscli2
+    flyctl
+    ffmpeg
+    yt-dlp
+    zathura
+    vscode
+    zoom-us
+  ] ++ (lib.optionals pkgs.stdenv.isDarwin [
+    smimesign
+    cocoapods
+    lima
+    colima
+    iterm2
+    raycast
+    iina
+    osu-lazer-bin
+  ]) ++ (lib.optionals pkgs.stdenv.isLinux [
+    mpv
+  ]);
+}
