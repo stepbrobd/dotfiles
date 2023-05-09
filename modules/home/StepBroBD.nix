6ada6a6ba9972666f6baa4904f17e09c030d2f1f
StepBroBD <Hi@StepBroBD.com>
2023-05-09 13:58:29 -0600

    rearrange

--- modules/home/StepBroBD.nix
+++ modules/home/StepBroBD.nix
@@ -31,10 +31,10 @@ in
   ]
     (system:
       inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
         modules = [
           configurations
         ];
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
       }
     ) // { inherit homeModule; }
 )
