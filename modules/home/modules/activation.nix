3ccadf4508c0af6199b5a97f441c3286ed4e3900
StepBroBD <Hi@StepBroBD.com>
2023-05-31 14:37:53 -0600

    add launch agent activation for darwin systems

--- modules/home/modules/activation.nix
+++ modules/home/modules/activation.nix
@@ -10,6 +10,16 @@
         export PATH="$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin" && PATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PATH}))')"
         curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/stepbrobd/dotfiles/master/scripts/setup.sh | zsh
       '';
+
+      launchd = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        #!/usr/bin/env nix-shell
+        #!nix-shell -i zsh -p curl perl
+
+        set -eo pipefail
+
+        export PATH="$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin" && PATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PATH}))')"
+        curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/stepbrobd/dotfiles/master/scripts/launchd.sh | zsh
+      '';
     };
   };
 }
