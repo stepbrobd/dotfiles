2312687e3df7e3c38b20e6309a3c88d4bc717e84
StepBroBD <Hi@StepBroBD.com>
2023-06-16 09:39:36 -0600

    remove devenv cache

--- modules/darwin/modules/configurations.nix
+++ modules/darwin/modules/configurations.nix
@@ -10,13 +10,11 @@
           "https://stepbrobd.cachix.org"
           "https://nix-community.cachix.org"
           "https://nixpkgs-update.cachix.org"
-          "https://devenv.cachix.org"
         ];
         trusted-public-keys = [
           "stepbrobd.cachix.org-1:Aa5jdkPVrCOvzaLTC0kVP5PYQ5BtNnLg1tG1Qa/QuE4="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "nixpkgs-update.cachix.org-1:6y6Z2JdoL3APdu6/+Iy8eZX2ajf09e4EE9SnxSML1W8="
-          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         ];
       };
       extraOptions = ''
@@ -24,7 +22,7 @@
         auto-optimise-store = true
         keep-derivations = true
         keep-outputs = true
-        keep-failed = false
+        keep-failed = true
         keep-going = true
       '';
     };
