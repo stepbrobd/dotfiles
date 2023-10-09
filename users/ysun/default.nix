b48153b5268d97b6858029ce29a5a782e5772682
Yifei Sun <ysun@hey.com>
2023-10-09 18:18:44 -0400

    add ssh public key

--- users/ysun/default.nix
+++ users/ysun/default.nix
@@ -9,6 +9,7 @@
     description = "Yifei Sun";
 
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKS575waJuCMJT8HsnAnWLhpX3/IcToBwgjf8iel5TmZ ysun@fwl-13"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw ysun@mbp-14"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVktHp6yjTknysVbU24K014tFKCIIM3/rWqZV591NRn ysun@mbp-16"
     ];
