d6fbf52af633c0a48929c55fb24ad931553c55d1
Yifei Sun <ysun@hey.com>
2023-10-08 20:27:35 -0400

    add fwl-13 key

--- secrets.nix
+++ secrets.nix
@@ -1,8 +1,10 @@
 let
+  fwl-13 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKS575waJuCMJT8HsnAnWLhpX3/IcToBwgjfBie15TmZ ysun@fwl-13";
   mbp-14 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw ysun@mbp-14";
   mbp-16 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVktHp6yjTknysVbU24K014tFKCIIM3/rWqZV591NRn ysun@mbp-16";
 in
 {
+  "secrets/fwl-13.age".publicKeys = [ fwl-13 ];
   "secrets/mbp-14.age".publicKeys = [ mbp-14 ];
   "secrets/mbp-16.age".publicKeys = [ mbp-16 ];
 }
