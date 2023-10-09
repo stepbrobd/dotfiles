3428239113f5430f5a5295fc0706924cc23da6bd
Yifei Sun <ysun@hey.com>
2023-10-08 21:17:17 -0400

    add pgp key

--- secrets.nix
+++ secrets.nix
@@ -4,6 +4,8 @@ let
   mbp-16 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVktHp6yjTknysVbU24K014tFKCIIM3/rWqZV591NRn ysun@mbp-16";
 in
 {
+  "secrets/pgp.age".publicKeys = [ fwl-13 mbp-14 mbp-16 ];
+
   "secrets/fwl-13.age".publicKeys = [ fwl-13 ];
   "secrets/mbp-14.age".publicKeys = [ mbp-14 ];
   "secrets/mbp-16.age".publicKeys = [ mbp-16 ];
