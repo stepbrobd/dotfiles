21bbf48664d81e98c5d47e9aeb7fbe8bf289be5b
Yifei Sun <ysun@hey.com>
2023-10-09 18:18:33 -0400

    add activation script

--- users/ysun/activation.nix
+++ users/ysun/activation.nix
@@ -5,20 +5,9 @@
 }:
 
 {
-  age.secrets.pgp.file = ../../secrets/pgp.age;
   home.activation = {
-    importPGPKeys =
-      let
-        replace =
-          if pkgs.stdenv.isLinux
-          then builtins.replaceStrings [ "$XDG_RUNTIME_DIR" ] [ "\${XDG_RUNTIME_DIR}" ]
-          else if pkgs.stdenv.isDarwin
-          then builtins.replaceStrings [ "$(getconf DARWIN_USER_TEMP_DIR)" ] [ "\${$(getconf DARWIN_USER_TEMP_DIR)}" ]
-          else abort "Unsupported OS";
-        path = replace config.age.secrets.pgp.path;
-      in
-      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        ${pkgs.gnupg}/bin/gpg --import ${path}
-      '';
+    hushlogin = lib.hm.dag.entryAnywhere ''
+      $DRY_RUN_CMD touch ${config.home.homeDirectory}/.hushlogin
+    '';
   };
 }
