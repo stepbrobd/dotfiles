c6e71377f4aa83616ff64837c4641de7d8894a7c
Yifei Sun <ysun@hey.com>
2023-10-10 12:54:15 -0400

    remove duplicate --time flag

--- modules/greetd/default.nix
+++ modules/greetd/default.nix
@@ -12,7 +12,7 @@
     settings.default_session = {
       vt = "next";
       user = "greeter";
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${config.programs.hyprland.package}/bin/Hyprland --time --remember --remember-session --remember-user-session --asterisks --power-shutdown shutdown now --power-reboot reboot";
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd ${config.programs.hyprland.package}/bin/Hyprland --time --remember --remember-session --remember-user-session --asterisks --power-shutdown shutdown now --power-reboot reboot";
     };
   };
 }
