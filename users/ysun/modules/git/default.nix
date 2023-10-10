4d33592dd3444590e7eef795836ef65cc0c7d2cd
Yifei Sun <ysun@hey.com>
2023-10-09 22:51:14 -0400

    git

--- users/ysun/modules/git/default.nix
+++ users/ysun/modules/git/default.nix
@@ -12,5 +12,22 @@
     package = pkgs.gitFull;
     userName = "Yifei Sun";
     userEmail = "ysun+git@stepbrobd.com";
+    signing = {
+      signByDefault = true;
+      key = null;
+    };
+    extraConfig = {
+      color.ui = true;
+      core.autocrlf = "input";
+      core.sshCommand = "ssh -o TCPKeepAlive=yes -o ServerAliveInterval=60";
+      filter.lfs.clean = "git-lfs clean -- %f";
+      filter.lfs.smudge = "git-lfs smudge -- %f";
+      init.defaultBranch = "master";
+      push.autoSetupRemote = true;
+      push.default = "upstream";
+      pull.ff = "only";
+      pull.rebase = false;
+      submodule.recurse = true;
+    };
   };
 }
