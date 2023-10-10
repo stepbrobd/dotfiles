29b74dd02ca7aa5c72bd985ccd240e7763647348
Yifei Sun <ysun@hey.com>
2023-10-09 22:00:42 -0400

    fix zsh p10k

--- users/ysun/modules/zsh/default.nix
+++ users/ysun/modules/zsh/default.nix
@@ -41,5 +41,13 @@
         }
       ];
     };
+    initExtra = ''
+      source ${./p10k.zsh}
+    '';
+
+    initExtraFirst = ''
+      P10K_INSTANT_PROMPT="$XDG_CACHE_HOME/p10k-instant-prompt-''${(%):-%n}.zsh"
+      [[ ! -r "$P10K_INSTANT_PROMPT" ]] || source "$P10K_INSTANT_PROMPT"
+    '';
   };
 }
