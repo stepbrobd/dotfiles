dfbc1581e9043ef55f7faab8ddf6d8fcac9f014b
Yifei Sun <ysun@hey.com>
2023-10-11 23:07:44 -0400

    nvim lualine

--- users/ysun/modules/neovim/default.nix
+++ users/ysun/modules/neovim/default.nix
@@ -12,6 +12,7 @@
 
     plugins = with pkgs.vimPlugins; [
       nord-nvim
+      lualine-nvim
     ];
 
     extraConfig = ''
@@ -19,6 +20,31 @@
 
       set number
       set relativenumber
+    '';
+
+    extraLuaConfig = ''
+            require("lualine").setup {
+              options = {
+      	  theme = "auto",
+                icons_enabled = true,
+      	  component_separators = {
+      	    left = "",
+      	    right = "",
+      	  },
+      	  section_separators = {
+      	    left = "",
+      	    right = "",
+      	  },
+      	},
+              sections = {
+      	  lualine_a = { "mode" },
+      	  lualine_b = { "branch", "diff", "diagnostics" },
+      	  lualine_c = {},
+      	  lualine_x = {},
+      	  lualine_y = { "filetype", "encoding" },
+      	  lualine_z = { "location" },
+      	},
+            }
     '';
   };
 }
