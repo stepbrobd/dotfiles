d84ed64219d7326724ce88faaa42ddd1315c52dd
Yifei Sun <ysun@hey.com>
2023-10-13 00:17:28 -0400

    add options

--- modules/greetd/default.nix
+++ modules/greetd/default.nix
@@ -12,7 +12,17 @@
     settings.default_session = {
       vt = "next";
       user = "greeter";
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd ${config.programs.hyprland.package}/bin/Hyprland --time --remember --asterisks --power-shutdown shutdown now --power-reboot reboot";
+      command = lib.strings.concatStringsSep " " [
+        "${pkgs.greetd.tuigreet}/bin/tuigreet"
+        "--time"
+        "--remember"
+        "--user-menu"
+        "--asterisks"
+        "--power-no-setsid"
+        "--power-shutdown shutdown now"
+        "--power-reboot reboot"
+        "--cmd ${config.programs.hyprland.package}/bin/Hyprland"
+      ];
     };
   };
 }
