486dd2c5cde3a397930fbe172c9be00faeeb4c21
Yifei Sun <ysun@hey.com>
2023-10-13 00:03:36 -0400

    switch back to tuigreet

--- modules/greetd/default.nix
+++ modules/greetd/default.nix
@@ -10,28 +10,9 @@
   services.greetd = {
     enable = true;
     settings.default_session = {
+      vt = "next";
       user = "greeter";
-      command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.regreet}/bin/regreet";
-    };
-  };
-
-  programs.regreet = {
-    enable = true;
-    settings = {
-      GTK = {
-        theme_name = "Nordic";
-        icon_theme_name = "Nordzy-dark";
-        font_name = "Noto Sans Regular 12";
-        application_prefer_dark_theme = true;
-      };
-      background = {
-        path = ./wallpaper.jpg;
-        fit = "Cover";
-      };
-      commands = {
-        reboot = "systemctl reboot";
-        shutdown = "systemctl poweroff";
-      };
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd ${config.programs.hyprland.package}/bin/Hyprland --time --remember --asterisks --power-shutdown shutdown now --power-reboot reboot";
     };
   };
 }
