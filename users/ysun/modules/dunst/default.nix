23f69de585b467edfc7dff207b3467a548327e6b
Yifei Sun <ysun@hey.com>
2023-10-13 13:55:17 -0400

    dunst colors

--- users/ysun/modules/dunst/default.nix
+++ users/ysun/modules/dunst/default.nix
@@ -12,15 +12,26 @@
     settings = {
       global = {
         sort = true;
+        shrink = "no";
+        markup = "yes";
+        transparency = 0;
         ellipsize = true;
         alignment = "center";
         origin = "top-center";
         font = "Noto Sans 10";
+
         frame_width = 1;
-        transparency = 5;
-        corner_radius = 5;
-        icon_corner_radius = 5;
-        progress_bar_corner_radius = 5;
+        corner_radius = 4;
+        icon_corner_radius = 4;
+        progress_bar_corner_radius = 4;
+        padding = 8;
+        horizontal_padding = 8;
+        separator_height = 2;
+
+        background = "#2e3440";
+        foreground = "#d8dee9";
+        frame_color = "#4c566a";
+        separator_color = "frame";
       };
     };
   };
