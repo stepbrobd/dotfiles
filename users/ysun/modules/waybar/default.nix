7fb2c3a35f9842a2827874dc7c3db6ff6c63abcf
Yifei Sun <ysun@hey.com>
2023-10-13 15:14:53 -0400

    waybar

--- users/ysun/modules/waybar/default.nix
+++ users/ysun/modules/waybar/default.nix
@@ -9,5 +9,24 @@
 {
   programs.waybar = {
     enable = true;
+
+    settings = {
+      layer = "top";
+      position = "top";
+
+      modules-left = [ ];
+      modules-center = [ "hyprland/window" ];
+      modules-right = [ ];
+
+      "hyprland/workspaces" = {
+        format = "{icon}";
+        on-click = "activate";
+        active-only = false;
+      };
+    };
+
+    style = ''
+    
+    '';
   };
 }
