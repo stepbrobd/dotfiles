16b3d7594e1e65f6854b7999374a000d7ac2d622
Yifei Sun <ysun@hey.com>
2023-10-14 00:14:35 -0400

    enable wireplumber

--- systems/ysun.co/fwl-13/hardware.nix
+++ systems/ysun.co/fwl-13/hardware.nix
@@ -65,11 +65,12 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
     alsa = {
       enable = true;
       support32Bit = true;
     };
-    pulse.enable = true;
   };
 
   # bluetooth
