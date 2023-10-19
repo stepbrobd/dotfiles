ecf953205de3aa83ad1cf73c5538518532d8a97f
Yifei Sun <ysun@hey.com>
2023-10-19 17:16:22 -0400

    tmux config

--- users/ysun/modules/tmux/default.nix
+++ users/ysun/modules/tmux/default.nix
@@ -15,13 +15,34 @@
     clock24 = true;
     terminal = "screen-256color";
 
-    plugins = [
-
+    plugins = with pkgs.tmuxPlugins; [
+      nord
+      sensible
+      resurrect
     ];
 
     extraConfig = ''
+      set -g default-terminal "tmux-256color"
+      set -ag terminal-overrides ",xterm-256color:RGB"
+      set -g mouse on
+      set -g status on
+      set -g status-position top
+      set -g status-justify centre
+
       set -g base-index 1
       setw -g pane-base-index 1
+
+      set -g allow-rename on
+      setw -g automatic-rename
+      set-option -g renumber-windows on
+
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+      
+      bind r source-file ~/.tmux.conf \; display-message "tmux: config reloaded"
+      bind s set-window-option synchronize-panes\; display-message "synchronize-panes: #{?pane_synchronized,on,off}"
     '';
   };
 }
