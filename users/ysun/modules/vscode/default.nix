48d53407118a8d1401b0ae3049db09a2bb8f2e2e
Yifei Sun <ysun@hey.com>
2023-11-08 10:33:20 -0500

    add vscode extensions

--- users/ysun/modules/vscode/default.nix
+++ users/ysun/modules/vscode/default.nix
@@ -15,9 +15,59 @@
     mutableExtensionsDir = false;
 
     extensions = with pkgs.vscode-extensions; [
+      arcticicestudio.nord-visual-studio-code
+      b4dm4n.vscode-nixpkgs-fmt
+      bbenoist.nix
+      bradlc.vscode-tailwindcss
+      charliermarsh.ruff
+      davidanson.vscode-markdownlint
+      dbaeumer.vscode-eslint
+      denoland.vscode-deno
+      dotjoshjohnson.xml
+      eamodio.gitlens
+      esbenp.prettier-vscode
+      foxundermoon.shell-format
+      github.codespaces
+      github.copilot
+      github.copilot-chat
+      github.vscode-github-actions
+      github.vscode-pull-request-github
+      golang.go
+      grapecity.gc-excelviewer
+      hashicorp.terraform
+      haskell.haskell
+      james-yu.latex-workshop
+      justusadam.language-haskell
+      llvm-vs-code-extensions.vscode-clangd
+      marp-team.marp-vscode
+      mechatroner.rainbow-csv
+      ms-azuretools.vscode-docker
+      ms-python.isort
+      ms-python.python
+      ms-python.vscode-pylance
+      ms-toolsai.jupyter
+      ms-toolsai.jupyter-renderers
+      ms-toolsai.vscode-jupyter-cell-tags
+      ms-toolsai.vscode-jupyter-slideshow
+      ms-vscode-remote.remote-containers
+      ms-vscode-remote.remote-ssh
+      ms-vscode.cmake-tools
+      ms-vscode.cpptools
+      ms-vsliveshare.vsliveshare
+      nvarner.typst-lsp
+      redhat.vscode-yaml
+      rust-lang.rust-analyzer
+      shd101wyy.markdown-preview-enhanced
+      streetsidesoftware.code-spell-checker
+      tamasfe.even-better-toml
+      tomoki1207.pdf
+      unifiedjs.vscode-mdx
+      vadimcn.vscode-lldb
+      vscode-icons-team.vscode-icons
+      vscodevim.vim
+      yzhang.markdown-all-in-one
     ];
 
-    userSettings = {
-    };
+    userSettings = { };
   };
 }
