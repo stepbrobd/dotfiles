093fa7e55ce0d870495e8a0482c9b8f3decf00f1
Yifei Sun <ysun@hey.com>
2023-11-11 15:48:38 -0500

    test vaultwarden

--- secrets.nix
+++ secrets.nix
@@ -2,13 +2,17 @@ let
   fwl-13 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBVZ9mzYNxccuh3uQR7Hly4KjhbRh4s6UlGQe2GjMtIC ysun@fwl-13";
   mbp-14 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw ysun@mbp-14";
   mbp-16 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVktHp6yjTknysVbU24K014tFKCIIM3/rWqZV591NRn ysun@mbp-16";
+
+  vault = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDsxaB3t68iDrvOMFnyIaktZ/cr4aidN8g/VxtJwAhjR ysun@vault";
+
+  ysun = [ fwl-13 mbp-14 mbp-16 ];
 in
 {
-  "secrets/pgp.age".publicKeys = [ fwl-13 mbp-14 mbp-16 ];
+  "secrets/pgp.age".publicKeys = ysun;
 
   "secrets/fwl-13.age".publicKeys = [ fwl-13 ];
   "secrets/mbp-14.age".publicKeys = [ mbp-14 ];
   "secrets/mbp-16.age".publicKeys = [ mbp-16 ];
 
-  "secrets/vaultwarden.age".publicKeys = [ fwl-13 mbp-14 mbp-16 ];
+  "secrets/vaultwarden.age".publicKeys = ysun ++ [ vault ];
 }
