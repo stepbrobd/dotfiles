adb3ad37564cf0c16aea99496644435ac6eb3924
Yifei Sun <ysun@hey.com>
2023-11-14 12:10:01 -0500

    slack webrtc pipewire capturer

--- users/ysun/modules/hyprland/default.nix
+++ users/ysun/modules/hyprland/default.nix
@@ -45,10 +45,10 @@
       exec-once = wpaperd &
 
       exec-once = hyprctl dispatch exec "[workspace 1 silent] ${config.home.sessionVariables.TERM}"
-      exec-once = hyprctl dispatch exec "[workspace 2 silent] ${config.home.sessionVariables.BROWSER} --enable-features=UseOzonePlatform --ozone-platform=wayland"
-      exec-once = hyprctl dispatch exec "[workspace 3 silent] slack --enable-features=UseOzonePlatform --ozone-platform=wayland"
-      exec-once = hyprctl dispatch exec "[workspace 3 silent] discord --enable-features=UseOzonePlatform --ozone-platform=wayland"
-      exec-once = hyprctl dispatch exec "[workspace 3 silent] spotify --enable-features=UseOzonePlatform --ozone-platform=wayland"
+      exec-once = hyprctl dispatch exec "[workspace 2 silent] ${config.home.sessionVariables.BROWSER} --ozone-platform=wayland --enable-features=UseOzonePlatform"
+      exec-once = hyprctl dispatch exec "[workspace 3 silent] slack --ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+      exec-once = hyprctl dispatch exec "[workspace 3 silent] discord --ozone-platform=wayland --enable-features=UseOzonePlatform"
+      exec-once = hyprctl dispatch exec "[workspace 3 silent] spotify --ozone-platform=wayland --enable-features=UseOzonePlatform"
 
       general {
         gaps_in = 4
