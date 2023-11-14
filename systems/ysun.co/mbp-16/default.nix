e7ed348c118d2124179dc9cfe6f972793c6b9f21
Yifei Sun <ysun@hey.com>
2023-11-14 12:59:31 -0500

    mbp-16

---
+++ systems/ysun.co/mbp-16/default.nix
@@ -0,0 +1,35 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+
+{
+  time.timeZone = "America/New_York";
+
+  networking = {
+    hostName = "mbp-16";
+    localHostName = "Yifeis-MacBook-Pro-16";
+    computerName = "Yifei's MacBook Pro 16\"";
+    knownNetworkServices = [
+      "Tailscale Tunnel"
+
+      "Wi-Fi"
+      "Bluetooth PAN"
+
+      "iPhone USB"
+      "Ethernet Adaptor"
+      "Belkin USB-C LAN"
+      "USB 10/100/1000 LAN"
+
+      "Thunderbolt Bridge"
+      "Thunderbolt Ethernet"
+      "Thunderbolt Ethernet Slot 1"
+      "Thunderbolt Ethernet Slot 2"
+      "Thunderbolt Ethernet Slot 3"
+      "Thunderbolt Ethernet Slot 4"
+    ];
+  };
+
+  security.pam.enableSudoTouchIdAuth = true;
+}
