44bc70d18613e035167de28fd3006529061b24ca
Yifei Sun <ysun@hey.com>
2023-12-16 13:42:02 -0500

    temprarily remove vscode typst lsp, zathura, allow eol electron 25.9.0 due to build errors

--- modules/nixpkgs/default.nix
+++ modules/nixpkgs/default.nix
@@ -10,5 +10,8 @@
   nixpkgs.config = {
     allowUnfree = true;
     allowUnfreePredicate = (_: true);
+
+    # idk what package is depending on electron-25.9.0
+    permittedInsecurePackages = [ "electron-25.9.0" ];
   };
 }
