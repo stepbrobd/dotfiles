3403d0b9b8310d102f7b4bc78799f0424e37c80a
Yifei Sun <ysun@hey.com>
2023-12-17 18:21:20 -0500

    change from gcp image to nixos-infect

--- systems/as10779.net/zrh1/hardware.nix
+++ systems/as10779.net/zrh1/hardware.nix
@@ -1,12 +1,29 @@
-{ config
-, lib
-, pkgs
-, modulesPath
+{ modulesPath
 , ...
 }:
 
 {
   imports = [
-    "${modulesPath}/virtualisation/google-compute-image.nix"
+    "${modulesPath}/profiles/qemu-guest.nix"
   ];
+
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+
+  boot = {
+    loader.grub = {
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      device = "nodev";
+    };
+    initrd = {
+      availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+      kernelModules = [ "nvme" ];
+    };
+  };
+
+  fileSystems = {
+    "/" = { device = "/dev/sda1"; fsType = "ext4"; };
+    "/boot" = { device = "/dev/disk/by-uuid/D0F3-B58A"; fsType = "vfat"; };
+  };
 }
