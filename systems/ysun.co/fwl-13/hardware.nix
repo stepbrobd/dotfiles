64857d46c2f375fce741b359c046a300f00eaada
Yifei Sun <ysun@hey.com>
2023-12-20 17:31:35 -0500

    tlp limit charge to 70%~75%

--- systems/ysun.co/fwl-13/hardware.nix
+++ systems/ysun.co/fwl-13/hardware.nix
@@ -45,6 +45,8 @@
     settings = {
       # run `ls /sys/class/power_supply` to check available power supplies
       # run `tlp fullcharge` to charge to 100%
+      STOP_CHARGE_THRESH_BAT0 = 70;
+      START_CHARGE_THRESH_BAT0 = 75;
       STOP_CHARGE_THRESH_BAT1 = 70;
       START_CHARGE_THRESH_BAT1 = 75;
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
