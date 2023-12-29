734ba117161ed6b9fc3022f3936ceabf43f00c61
Yifei Sun <ysun@hey.com>
2023-12-28 22:24:03 -0500

    zrh-1: add caddy server alias

--- systems/as10779.net/zrh-1/caddy.nix
+++ systems/as10779.net/zrh-1/caddy.nix
@@ -54,6 +54,11 @@
       }
     '';
 
+    virtualHosts."*.internal.center".extraConfig = ''
+      import common
+      redir https://internal.center{uri}
+    '';
+
     virtualHosts."stats.ysun.co".extraConfig = ''
       import common
       reverse_proxy ${toString config.services.plausible.server.listenAddress}:${toString config.services.plausible.server.port} {
@@ -62,10 +67,29 @@
       }
     '';
 
-    virtualHosts."ysun.co".extraConfig = ''
-      import common
-      reverse_proxy localhost:8000
-    '';
+    virtualHosts."ysun.co" = {
+      extraConfig = ''
+        import common
+        reverse_proxy localhost:8000
+      '';
+      serverAliases = [
+        "*.ysun.co"
+        "as10779.net"
+        "*.as10779.net"
+        "churn.cards"
+        "*.churn.cards"
+        "metaprocessor.org"
+        "*.metaprocessor.org"
+        "stepbrobd.com"
+        "*.stepbrobd.com"
+        "xdg.sh"
+        "*.xdg.sh"
+        "yifei-s.com"
+        "*.yifei-s.com"
+        "ysun.life"
+        "*.ysun.life"
+      ];
+    };
   };
 
   age.secrets.cloudflare = {
