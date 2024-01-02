ef58e179dba6517a613780a1f376fce8a00017d0
Yifei Sun <ysun@hey.com>
2024-01-01 21:06:40 -0500

    remove names from ssh keys

--- users/ysun/default.nix
+++ users/ysun/default.nix
@@ -19,9 +19,9 @@
       else abort "Unsupported OS";
 
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBVZ9mzYNxccuh3uQR7Hly4KjhbRh4s6UlGQe2GjMtIC ysun@fwl-13"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw ysun@mbp-14"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVktHp6yjTknysVbU24K014tFKCIIM3/rWqZV591NRn ysun@mbp-16"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBVZ9mzYNxccuh3uQR7Hly4KjhbRh4s6UlGQe2GjMtIC" # ysun@fwl-13
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw" # ysun@mbp-14
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVktHp6yjTknysVbU24K014tFKCIIM3/rWqZV591NRn" # ysun@mbp-16
     ];
   } // lib.optionalAttrs pkgs.stdenv.isLinux {
     isNormalUser = true;
