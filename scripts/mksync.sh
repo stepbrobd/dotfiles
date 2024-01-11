963d03fa8c6d778c097e2a34b65e109b9dcd68e6
Yifei Sun <ysun@hey.com>
2024-01-11 17:22:08 -0500

    sync forks automatically

---
+++ scripts/mksync.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p jq gh
+
+set -euo pipefail
+
+forks=$(gh repo list --limit 1000 --fork --json 'owner,name,parent')
+
+echo "$forks" | nix run nixpkgs#jq -- -c '.[]' | while read -r repo; do
+    own=$(echo $repo | jq -r '.owner.login + "/" + .name')
+    upstream=$(echo $repo | jq -r '.parent.owner.login + "/" + .parent.name')
+    gh repo sync $own --source $upstream
+done
