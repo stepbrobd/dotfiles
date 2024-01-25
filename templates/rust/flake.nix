6c6c269b18f0f87d5c0da142f778a13819b97387
Yifei Sun <ysun@hey.com>
2024-01-25 16:09:35 -0500

    template: rust

---
+++ templates/rust/flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "<NAME>";
+        inherit ((pkgs.lib.importTOML ./Cargo.toml).package) version;
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = self.packages.${system}.default;
+      };
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+        ];
+      };
+
+      formatter = pkgs.writeShellScriptBin "formatter" ''
+        set -eoux pipefail
+        shopt -s globstar
+        ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
+        ${pkgs.rustfmt}/bin/rustfmt **/*.rs
+      '';
+    });
+}
