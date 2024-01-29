05901212aac132cc9a83bdfd859c26e5278ec04b
Yifei Sun <ysun@hey.com>
2024-01-29 09:27:09 -0500

    ssh: add server cat

--- users/ysun/default.nix
+++ users/ysun/default.nix
@@ -22,6 +22,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBVZ9mzYNxccuh3uQR7Hly4KjhbRh4s6UlGQe2GjMtIC" # ysun@fwl-13
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw" # ysun@mbp-14
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVktHp6yjTknysVbU24K014tFKCIIM3/rWqZV591NRn" # ysun@mbp-16
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQCjwNmB60FQhDncIyX/wCRAPIlLD5KLAGrgAdt4xGw" # ysun@srv-ct
     ];
   } // lib.optionalAttrs pkgs.stdenv.isLinux {
     isNormalUser = true;
