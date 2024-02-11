1c24de071f41efc820ec9b95391c2778892ca683
Yifei Sun <ysun@hey.com>
2024-02-11 16:58:41 -0500

    git: config

--- users/ysun/modules/git/default.nix
+++ users/ysun/modules/git/default.nix
@@ -14,12 +14,18 @@
     userEmail = "ysun@hey.com";
     signing = {
       signByDefault = true;
-      key = null;
+      key = "D973170F9B86DB70";
     };
+
     extraConfig = {
+      branch.sort = "-committerdate";
       color.ui = true;
+      column.ui = "auto";
       core.autocrlf = "input";
+      core.fsmonitor = true;
       core.sshCommand = "ssh -o TCPKeepAlive=yes -o ServerAliveInterval=60";
+      core.untrackedcache = true;
+      fetch.writeCommitGraph = true;
       filter.lfs.clean = "git-lfs clean -- %f";
       filter.lfs.smudge = "git-lfs smudge -- %f";
       init.defaultBranch = "master";
