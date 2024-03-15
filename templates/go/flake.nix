7e666f8ff176b5581eabb1801b1d17e162b6fb90
Yifei Sun <ysun@hey.com>
2024-03-15 13:57:41 -0400

    templates: format

--- templates/go/flake.nix
+++ templates/go/flake.nix
@@ -14,42 +14,42 @@
     , nixpkgs
     , flake-utils
     , gomod2nix
-    }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          gomod2nix.overlays.default
-        ];
-      };
-    in
-    {
-      packages.default = pkgs.buildGoApplication {
-        pname = "<NAME>";
-        version = self.shortRev or self.dirtyShortRev;
-        src = ./.;
-        modules = ./gomod2nix.toml;
-      };
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ gomod2nix.overlays.default ];
+        };
+      in
+      {
+        packages.default = pkgs.buildGoApplication {
+          pname = "<NAME>";
+          version = self.shortRev or self.dirtyShortRev;
+          src = ./.;
+          modules = ./gomod2nix.toml;
+        };
 
-      apps.default = flake-utils.lib.mkApp {
-        drv = self.packages.${system}.default;
-      };
+        apps.default = flake-utils.lib.mkApp { drv = self.packages.${system}.default; };
 
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          delve
-          go
-          gopls
-          gotools
-          go-tools
-          gomod2nix.packages.${system}.default
-        ];
-      };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            delve
+            go
+            gopls
+            gotools
+            go-tools
+            gomod2nix.packages.${system}.default
+          ];
+        };
 
-      formatter = pkgs.writeShellScriptBin "formatter" ''
-        set -eoux pipefail
-        ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
-        ${pkgs.go}/bin/gofmt -s -w .
-      '';
-    });
+        formatter = pkgs.writeShellScriptBin "formatter" ''
+          set -eoux pipefail
+          ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
+          ${pkgs.go}/bin/gofmt -s -w .
+        '';
+      }
+    );
 }
--- templates/python/flake.nix
+++ templates/python/flake.nix
@@ -13,48 +13,52 @@
     , nixpkgs
     , flake-utils
     , pyproject
-    }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-          allowUnfreePredicate = (_: true);
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            allowUnfreePredicate = (_: true);
+          };
         };
-      };
-      python3 = pkgs.python311;
-    in
-    {
-      packages.default = python.pkgs.buildPythonPackage (
-        project.renderers.buildPythonPackage { inherit python; }
-      );
+        python3 = pkgs.python311;
+      in
+      {
+        packages.default = python.pkgs.buildPythonPackage (
+          project.renderers.buildPythonPackage { inherit python; }
+        );
 
-      apps.default = flake-utils.lib.mkApp {
-        drv = self.packages.${system}.default;
-      };
+        apps.default = flake-utils.lib.mkApp { drv = self.packages.${system}.default; };
 
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          direnv
-          git
-          nix-direnv
-          ruff
-        ];
-        venvDir = "./.venv";
-        buildInputs = [ python ] ++ (with python.pkgs; [
-          venvShellHook
-          setuptools
-          wheel
-        ]);
-        shellHook = ''
-          pip --disable-pip-version-check install -e .
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            direnv
+            git
+            nix-direnv
+            ruff
+          ];
+          venvDir = "./.venv";
+          buildInputs =
+            [ python ]
+            ++ (with python.pkgs; [
+              venvShellHook
+              setuptools
+              wheel
+            ]);
+          shellHook = ''
+            pip --disable-pip-version-check install -e .
+          '';
+        };
+
+        formatter = pkgs.writeShellScriptBin "formatter" ''
+          set -eoux pipefail
+          ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
+          ${pkgs.ruff}/bin/ruff --fix --unsafe-fixes .
         '';
-      };
-
-      formatter = pkgs.writeShellScriptBin "formatter" ''
-        set -eoux pipefail
-        ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
-        ${pkgs.ruff}/bin/ruff --fix --unsafe-fixes .
-      '';
-    });
+      }
+    );
 }
