e72611cf4977c26d54e3aeaa9f1300e1c1f7cc59
Yifei Sun <ysun@hey.com>
2024-03-16 19:55:24 -0400

    flake: add {nixos,darwin,hm} modules to outputs

--- flake.nix
+++ flake.nix
@@ -13,6 +13,117 @@
     {
       schemas = inputs.schemas.schemas;
 
+      nixosModules = {
+        common = import ./modules/common;
+        graphical = import ./modules/nixos/graphical.nix;
+        minimal = import ./modules/nixos/minimal.nix;
+      };
+
+      darwinModules = {
+        common = import ./modules/common;
+        default = import ./modules/darwin;
+      };
+
+      hmModules.ysun = {
+        darwin = import ./modules/home/ysun/darwin.nix;
+        graphical = import ./modules/home/ysun/graphical.nix;
+        linux = import ./modules/home/ysun/linux.nix;
+        minimal = import ./modules/home/ysun/minimal.nix;
+      };
+
+      # SSDNodes Performance, 8 vCPU, 32GB RAM, 640GB Storage
+      nixosConfigurations.nrt-1 = lib.mkSystem rec {
+        systemType = "nixos";
+        hostPlatform = "x86_64-linux";
+        systemStateVersion = "24.05";
+        hmStateVersion = systemStateVersion;
+        systemConfig = ./systems/nixos/nrt-1;
+        username = "ysun";
+        extraModules = [
+          inputs.srvos.nixosModules.server
+          outputs.nixosModules.common
+          outputs.nixosModules.minimal
+        ];
+        extraHMModules = [ outputs.hmModules.ysun.minimal ];
+      };
+
+      # EC2 T3.Large, 2 vCPU, 8GB RAM, 30GB Storage
+      nixosConfigurations.zrh-1 = lib.mkSystem rec {
+        systemType = "nixos";
+        hostPlatform = "x86_64-linux";
+        systemStateVersion = "24.05";
+        hmStateVersion = systemStateVersion;
+        systemConfig = ./systems/nixos/zrh-1;
+        username = "ysun";
+        extraModules = [
+          inputs.srvos.nixosModules.server
+          outputs.nixosModules.common
+          outputs.nixosModules.minimal
+        ];
+        extraHMModules = [ outputs.hmModules.ysun.minimal ];
+      };
+
+      # EC2 T3.Micro, 2 vCPU, 1GB RAM, 30GB Storage
+      nixosConfigurations.zrh-2 = lib.mkSystem rec {
+        systemType = "nixos";
+        hostPlatform = "x86_64-linux";
+        systemStateVersion = "24.05";
+        hmStateVersion = systemStateVersion;
+        systemConfig = ./systems/nixos/zrh-2;
+        username = "ysun";
+        extraModules = [
+          inputs.srvos.nixosModules.server
+          outputs.nixosModules.common
+          outputs.nixosModules.minimal
+        ];
+        extraHMModules = [ outputs.hmModules.ysun.minimal ];
+      };
+
+      # Framework Laptop 13, Intel Core i7-1360P, 64GB RAM, 1TB Storage
+      nixosConfigurations.fwl-13 = lib.mkSystem rec {
+        systemType = "nixos";
+        hostPlatform = "x86_64-linux";
+        systemStateVersion = "24.05";
+        hmStateVersion = systemStateVersion;
+        systemConfig = ./systems/nixos/fwl-13;
+        username = "ysun";
+        extraModules = [
+          inputs.disko.nixosModules.disko
+          inputs.lanzaboote.nixosModules.lanzaboote
+          inputs.nixos-generators.nixosModules.all-formats
+          inputs.nixos-hardware.nixosModules.common-hidpi
+          inputs.nixos-hardware.nixosModules.framework-13th-gen-intel
+          inputs.srvos.nixosModules.desktop
+          outputs.nixosModules.common
+          outputs.nixosModules.graphical
+        ];
+        extraHMModules = [ outputs.hmModules.ysun.linux ];
+      };
+
+      # MacBook Pro 14-inch, Apple M2 Max, 64GB RAM, 1TB Storage
+      darwinConfigurations.mbp-14 = lib.mkSystem {
+        systemType = "darwin";
+        hostPlatform = "aarch64-darwin";
+        systemStateVersion = 4;
+        hmStateVersion = "24.05";
+        systemConfig = ./systems/darwin/mbp-14;
+        username = "ysun";
+        extraModules = [ outputs.darwinModules.common outputs.darwinModules.default ];
+        extraHMModules = [ outputs.hmModules.ysun.darwin ];
+      };
+
+      # MacBook Pro 16-inch, Intel Core i9-9980HK, 32GB RAM, 2TB Storage
+      darwinConfigurations.mbp-16 = lib.mkSystem {
+        systemType = "darwin";
+        hostPlatform = "x86_64-darwin";
+        systemStateVersion = 4;
+        hmStateVersion = "24.05";
+        systemConfig = ./systems/darwin/mbp-16;
+        username = "ysun";
+        extraModules = [ outputs.darwinModules.common outputs.darwinModules.default ];
+        extraHMModules = [ outputs.hmModules.ysun.darwin ];
+      };
+
       overlays.default = import ./overlays { inherit lib; };
 
       templates = lib.mkDynamicAttrs {
@@ -25,10 +136,10 @@
 
       hydraJobs =
         let
-          configsFor = systemType: lib.mapAttrs (n: v: v.config.system.build.toplevel) self."${systemType}Configurations";
+          configsFor = systemType: lib.mapAttrs (n: v: v.config.system.build.toplevel) outputs."${systemType}Configurations";
         in
         {
-          inherit (self) packages devShells;
+          inherit (outputs) packages devShells;
           # nixosConfigurations = configsFor "nixos";
           # darwinConfigurations = configsFor "darwin";
         };
@@ -38,13 +149,16 @@
       let
         pkgs = import inputs.nixpkgs {
           inherit system;
-          overlays = [ self.overlays.default ];
+          overlays = [ outputs.overlays.default ];
         };
       in
       {
         packages = lib.mkDynamicAttrs {
           dir = ./packages;
-          fun = name: pkgs.callPackage (./packages/. + "/${name}") { };
+          fun = name: pkgs.callPackage (./packages/. + "/${name}") {
+            inputs = inputs;
+            outputs = outputs;
+          };
         };
 
         devShells.default = pkgs.mkShell {
