3a69615e3295d477d8247e09d920f82e614dbbda
Yifei Sun <ysun@hey.com>
2024-03-16 20:34:51 -0400

    nixvim: use old config

--- packages/nixvim/default.nix
+++ packages/nixvim/default.nix
@@ -26,39 +26,171 @@ inputs.nixvim.legacyPackages."${stdenv.hostPlatform.system}".makeNixvim {
     smarttab = true;
   };
 
-  plugins.lazy = {
-    enable = true;
-    plugins = with pkgs.vimPlugins; [
-      {
-        pkg = alpha-nvim;
-        dependencies = [ nvim-web-devicons telescope-nvim ];
-        config = ''
-          function()
-            local alpha = require("alpha")
-            local dashboard = require("alpha.themes.dashboard")
-            -- Set header
-            dashboard.section.header.val = {
-              [[     ███████╗███╗   ███╗ █████╗  ██████╗███████╗    ]],
-              [[     ██╔════╝████╗ ████║██╔══██╗██╔════╝██╔════╝    ]],
-              [[     █████╗  ██╔████╔██║███████║██║     ███████╗    ]],
-              [[     ██╔══╝  ██║╚██╔╝██║██╔══██║██║     ╚════██║    ]],
-              [[     ███████╗██║ ╚═╝ ██║██║  ██║╚██████╗███████║    ]],
-              [[     ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝    ]],
+  plugins = {
+    alpha = {
+      enable = true;
+      layout = [
+        {
+          type = "padding";
+          val = 10;
+        }
+        {
+          opts = {
+            hl = "Keyword";
+            position = "center";
+          };
+          type = "text";
+          val = "not";
+        }
+        {
+          type = "padding";
+          val = 1;
+        }
+        {
+          opts = {
+            hl = "Type";
+            position = "center";
+          };
+          type = "text";
+          val = [
+            "███████╗███╗   ███╗ █████╗  ██████╗███████╗"
+            "██╔════╝████╗ ████║██╔══██╗██╔════╝██╔════╝"
+            "█████╗  ██╔████╔██║███████║██║     ███████╗"
+            "██╔══╝  ██║╚██╔╝██║██╔══██║██║     ╚════██║"
+            "███████╗██║ ╚═╝ ██║██║  ██║╚██████╗███████║"
+            "╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝"
+          ];
+        }
+        {
+          type = "padding";
+          val = 5;
+        }
+        {
+          type = "group";
+          val = [
+            {
+              command = ":ene <BAR> startinsert <CR>";
+              desc = "  New File";
+              shortcut = "n";
             }
-            -- Set menu
-            dashboard.section.buttons.val = {
-              dashboard.button("n", "  New File", ":ene <BAR> startinsert <CR>"),
-              dashboard.button("f", "  Find File", ":Telescope find_files <CR>"),
-              dashboard.button("g", "󰱼  Live Grep", ":Telescope live_grep <CR>"),
-              dashboard.button("q", "  Quit", ":qa<CR>"),
+            {
+              command = ":Telescope find_files <CR>";
+              desc = "  Find File";
+              shortcut = "f";
             }
-            alpha.setup(dashboard.opts)
-          end
-        '';
-      }
-      {
-        pkg = Coqtail;
-      }
-    ];
+            {
+              command = ":Telescope live_grep <CR>";
+              desc = "  Live Grep";
+              shortcut = "g";
+            }
+            {
+              command = ":qa<CR>";
+              desc = "  Quit";
+              shortcut = "q";
+            }
+          ];
+        }
+        {
+          type = "padding";
+          val = 5;
+        }
+        {
+          opts = {
+            hl = "Keyword";
+            position = "center";
+          };
+          type = "text";
+          val = "its neovim";
+        }
+      ];
+    };
+    bufferline = {
+      enable = true;
+      alwaysShowBufferline = false;
+      diagnostics = "nvim_lsp";
+    };
+    cmp = {
+      enable = true;
+      settings = {
+        completion = {
+          autocomplete = [ "TextChanged" ];
+          completeopt = "menu,menuone,noselect,preview";
+          keywordLength = 1;
+        };
+        experimental = { ghost_text.hlgroup = "Comment"; };
+        mapping = {
+          "<cr>" = "cmp.mapping.confirm({ select = true })";
+          "<tab>" = "cmp.mapping.select_next_item()";
+          "<s-tab>" = "cmp.mapping.select_prev_item()";
+        };
+        snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+      };
+    };
+    cmp-buffer.enable = true;
+    cmp-cmdline.enable = true;
+    cmp-dictionary.enable = true;
+    cmp_luasnip.enable = true;
+    cmp-nvim-lsp.enable = true;
+    cmp-path.enable = true;
+    cmp-treesitter.enable = true;
+    copilot-cmp = {
+      enable = true;
+      event = [ "InsertEnter" "LspAttach" ];
+      fixPairs = true;
+    };
+    copilot-lua = {
+      enable = true;
+      filetypes."*" = true;
+      panel.enabled = false;
+      suggestion.enabled = false;
+    };
+    coq-nvim.enable = true;
+    coq-thirdparty.enable = true;
+    diffview.enable = true;
+    gitblame.enable = true;
+    gitsigns.enable = true;
+    lsp.enable = true;
+    lsp-format = {
+      enable = true;
+      lspServersToEnable = "all";
+    };
+    lspkind.enable = true;
+    lualine = {
+      enable = true;
+      iconsEnabled = true;
+      theme = "nord";
+      sections = {
+        lualine_a = [ "mode" ];
+        lualine_b = [ "branch" ];
+        lualine_c = [ "diff" "diagnostics" ];
+        lualine_x = [ "filetype" "encoding" ];
+        lualine_y = [ "progress" ];
+        lualine_z = [ "location" ];
+      };
+    };
+    luasnip.enable = true;
+    nix.enable = true;
+    nix-develop.enable = true;
+    noice.enable = true;
+    notify.enable = true;
+    oil.enable = true;
+    presence-nvim.enable = true;
+    rainbow-delimiters.enable = true;
+    spider.enable = true;
+    surround.enable = true;
+    telescope.enable = true;
+    toggleterm.enable = true;
+    treesitter.enable = true;
+    treesitter-context.enable = true;
+    treesitter-refactor.enable = true;
+    treesitter-textobjects.enable = true;
+    typst-vim = {
+      enable = true;
+      settings.pdf_viewer = "zathura";
+    };
   };
+
+  extraPlugins = with pkgs.vimPlugins; [
+    Coqtail
+  ];
 }
