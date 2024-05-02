20749b59281245da0ba16e78e02153d90a7fa08c
Yifei Sun <ysun@hey.com>
2024-05-02 01:45:52 -0400

    systems: change framework laptop tz

--- systems/nixos/framework/default.nix
+++ systems/nixos/framework/default.nix
@@ -8,7 +8,7 @@
   imports = [ ./hardware.nix ];
 
   i18n.defaultLocale = "en_US.UTF-8";
-  time.timeZone = "America/New_York";
+  time.timeZone = "Asia/Tokyo";
 
   services.openssh.enable = true;
 
