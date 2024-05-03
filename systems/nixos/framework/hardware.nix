ec4bdfd5dcfb8f9e8c32ca0abfe736110b514588
Yifei Sun <ysun@hey.com>
2024-05-03 14:02:04 +0900

    systems: enable low latency pipewire

--- systems/nixos/framework/hardware.nix
+++ systems/nixos/framework/hardware.nix
@@ -91,9 +91,17 @@
     enable = true;
     pulse.enable = true;
     wireplumber.enable = true;
+    jack.enable = true;
     alsa = {
       enable = true;
       support32Bit = true;
+    };
+
+    extraConfig.pipewire."92-low-latency".context.properties = {
+      default.clock.rate = 48000;
+      default.clock.quantum = 32;
+      default.clock.min-quantum = 32;
+      default.clock.max-quantum = 32;
     };
   };
 
