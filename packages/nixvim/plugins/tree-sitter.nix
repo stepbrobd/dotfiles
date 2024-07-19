9e627e8cc8903f23795fdd8f73f8c705a9108607
Yifei Sun <ysun@hey.com>
2024-07-19 22:37:21 +0900

    packages: nixvim enable todo-comments

--- packages/nixvim/plugins/tree-sitter.nix
+++ packages/nixvim/plugins/tree-sitter.nix
@@ -4,5 +4,8 @@
     treesitter-context.enable = true;
     treesitter-refactor.enable = true;
     treesitter-textobjects.enable = true;
+
+    # highlight comments
+    todo-comments.enable = true;
   };
 }
