d3204b18e85f417e07803cd270c3b1245866b646
Yifei Sun <ysun@hey.com>
2024-07-25 15:15:25 +0900

    secrets: add walberla host pub key

--- secrets.nix
+++ secrets.nix
@@ -1,10 +1,11 @@
 let
-  framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBVZ9mzYNxccuh3uQR7Hly4KjhbRh4s6UlGQe2GjMtIC"; # framework
-  macbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw"; # macbook
+  framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBVZ9mzYNxccuh3uQR7Hly4KjhbRh4s6UlGQe2GjMtIC";
+  macbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw";
 
-  odake = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAs7yXRw7MplEON8Fxk+teNkQdD4k/xOBsMdcxlXn6CC"; # host@nrt-1
-  lagern = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIATBSfq95xFwSazFiulDnNIZGqj0Aw7gEvw9LxF96PBv"; # host@zrh-1
-  bachtel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHLLHiyDq4GcetByxY0Gy2TeokjdXt2B2L20sFyZFlg"; # host@zrh-2
+  bachtel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHLLHiyDq4GcetByxY0Gy2TeokjdXt2B2L20sFyZFlg";
+  lagern = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIATBSfq95xFwSazFiulDnNIZGqj0Aw7gEvw9LxF96PBv";
+  odake = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAs7yXRw7MplEON8Fxk+teNkQdD4k/xOBsMdcxlXn6CC";
+  walberla = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkF4fUm58NR9Y16p6x24pOv06BXVKEAD6ONZidaLeFN";
 
   ysun = [ framework macbook ];
   kichinose = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC8LagJKcgCTZMrfGHTUE4y8B6g/5wgq+ufKHblSH81n" ];
@@ -17,11 +18,8 @@ in
   "secrets/hydra-notify.age".publicKeys = ysun ++ [ odake ];
 
   "secrets/cloudflare-kichinose.age".publicKeys = kichinose ++ [ lagern ];
-  "secrets/cloudflare-ysun.age".publicKeys = ysun ++ [
-    odake
-    lagern
-    bachtel
-  ];
+  "secrets/cloudflare-ysun.age".publicKeys = ysun ++ [ bachtel lagern odake walberla ];
+
   "secrets/plausible.adm.age".publicKeys = ysun ++ [ lagern ];
   "secrets/plausible.goo.age".publicKeys = ysun ++ [ lagern ];
   "secrets/plausible.mal.age".publicKeys = ysun ++ [ lagern ];
