6014711004081861e5cc16e577c37a14edbac481
Yifei Sun <ysun@hey.com>
2024-07-25 17:15:53 +0900

    lib: add deepMergeAttrsList

---
+++ lib/deep-merge-attrs-list.nix
@@ -0,0 +1,19 @@
+{ lib }:
+
+# https://stackoverflow.com/a/54505212/17129151
+let
+  inherit (lib) all concatLists head isAttrs isList last recursiveUpdate tail unique zipAttrsWith;
+in
+attrList:
+let
+  f = attrPath: zipAttrsWith (n: values:
+    if tail values == [ ]
+    then head values
+    else if all isList values
+    then unique (concatLists values)
+    else if all isAttrs values
+    then f (attrPath ++ [ n ]) values
+    else last values
+  );
+in
+f [ ] attrList
