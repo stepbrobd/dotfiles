eddb9b790b6dae7163dcde2b132ddef48623bcf1
Yifei Sun <ysun@hey.com>
2024-07-31 18:55:50 +0900

    modules: update trusted cache

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -38,22 +38,22 @@
 
       extra-substituters = [
         "https://cache.nixos.org"
-        "https://cache.ngi0.nixos.org"
-        "https://ngi.cachix.org"
+        "https://cache.garnix.io"
         "https://nix-community.cachix.org"
         "https://nixpkgs-update.cachix.org"
-        "https://cache.garnix.io"
+        "https://colmena.cachix.org"
+        "https://ngi.cachix.org"
         "https://stepbrobd.cachix.org"
         "https://cache.nixolo.gy"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
-        "ngi.cachix.org-1:n+CAL72ROC3qQuLxIHpV+Tw5t42WhXmMhprAGkRSrOw="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-update.cachix.org-1:6y6Z2JdoL3APdu6/+Iy8eZX2ajf09e4EE9SnxSML1W8="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
+        "ngi.cachix.org-1:n+CAL72ROC3qQuLxIHpV+Tw5t42WhXmMhprAGkRSrOw="
         "stepbrobd.cachix.org-1:Aa5jdkPVrCOvzaLTC0kVP5PYQ5BtNnLg1tG1Qa/QuE4="
         "cache.nixolo.gy:UDmjlw8J4sqDlBIPe5YnABPI1lkcJssN8niLozS2ltM="
       ];
