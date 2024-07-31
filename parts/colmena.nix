93b309d8a135880345443561b18cbf470a07d74a
Yifei Sun <ysun@hey.com>
2024-07-31 21:30:32 +0900

    modules: nixos config to colmena

--- parts/colmena.nix
+++ parts/colmena.nix
@@ -1,1 +1,19 @@
-{ }
+{ inputs, ... }:
+
+let
+  # colmena #161
+  mkColmenaFromNixOSConfigurations = conf:
+    {
+      meta = {
+        # description = "my personal machines";
+        nixpkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+
+        nodeNixpkgs = builtins.mapAttrs (_: value: value.pkgs) conf;
+
+        nodeSpecialArgs = builtins.mapAttrs (_: value: value._module.specialArgs) conf;
+      };
+    } // builtins.mapAttrs (_: value: { imports = value._module.args.modules; }) conf;
+in
+{
+  flake.colmena = mkColmenaFromNixOSConfigurations inputs.self.nixosConfigurations;
+}
