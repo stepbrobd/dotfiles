41723b03cfc8e545be076abac1c382bf93abf92b
Yifei Sun <ysun@hey.com>
2024-08-16 14:38:00 +0900

    packages: consolidate scripts into a package

---
+++ packages/stepbrobd/default.nix
@@ -0,0 +1,46 @@
+{ lib, stdenv, writeShellScriptBin, symlinkJoin, makeWrapper, bash, curl, gh, git, jq, python311 }:
+
+
+let
+  pname = "stepbrobd";
+  version = "2024.816.0";
+  meta = {
+    license = lib.licenses.mit;
+    mainProgram = "stepbrobd";
+    maintainers = [ lib.maintainers.stepbrobd ];
+  };
+
+  libexec = stdenv.mkDerivation {
+    pname = "${pname}-libexec";
+    inherit version;
+
+    src = ./.;
+
+    installPhase = ''
+      mkdir -p $out/{bin,libexec}
+      cp $src/*.{py,sh} $out/libexec
+    '';
+  };
+
+  stepbrobd = writeShellScriptBin pname ''
+    if [ "$1" = "discord" ]; then
+      shift
+      exec bash ${libexec}/libexec/discord.sh "$@"
+    elif [ "$1" = "repo" ]; then
+      shift
+      exec python ${libexec}/libexec/repo.py "$@"
+    elif [ "$1" = "sync" ]; then
+      shift
+      exec bash ${libexec}/libexec/sync.sh "$@"
+    else
+      echo "Usage: $0 {discord|repo|sync}"
+      exit 1
+    fi
+  '';
+in
+symlinkJoin {
+  name = "${pname}-${version}";
+  paths = [ libexec stepbrobd ] ++ [ bash curl gh git jq python311 ];
+  buildInputs = [ makeWrapper ];
+  postBuild = "wrapProgram $out/bin/${pname} --prefix PATH : $out/bin";
+}
