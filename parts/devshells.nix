3a19d77da393a0bf8871b339a9053117bec9d8a3
Yifei Sun <ysun@hey.com>
2024-08-16 14:43:35 +0900

    flake: add stepbrobd to devshells

--- parts/devshells.nix
+++ parts/devshells.nix
@@ -1,9 +1,11 @@
 {
-  perSystem = { pkgs, inputs', ... }: {
+  perSystem = { pkgs, inputs', self', ... }: {
     devShells.default = pkgs.mkShell {
       packages = with pkgs; [
         inputs'.agenix.packages.agenix
         inputs'.colmena.packages.colmena
+        self'.packages.stepbrobd
+
         direnv
         git
         nix-direnv
