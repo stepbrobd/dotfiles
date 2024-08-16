1a944fca8ffc3c17b6b382fa4c261fa4d97ebf29
Yifei Sun <ysun@hey.com>
2024-08-16 13:21:04 +0900

    ci: clean up

--- scripts/mksync.sh
+++ scripts/mksync.sh
@@ -5,8 +5,15 @@ set -euo pipefail
 
 forks=$(gh repo list --limit 1000 --fork --json 'owner,name,parent')
 
-echo "$forks" | nix run nixpkgs#jq -- -c '.[]' | while read -r repo; do
-    own=$(echo $repo | jq -r '.owner.login + "/" + .name')
-    upstream=$(echo $repo | jq -r '.parent.owner.login + "/" + .parent.name')
-    gh repo sync $own --source $upstream
+echo "$forks" | jq -c '.[]' | while read -r repo; do
+    own=$(echo "$repo" | jq -r '.owner.login + "/" + .name')
+    upstream=$(echo "$repo" | jq -r '.parent.owner.login + "/" + .parent.name')
+    echo "Syncing $own from $upstream"
+
+    if [ "$upstream" = "/" ]; then
+        echo "Skipped"
+    else
+        gh repo sync "$own" --source "$upstream"
+        echo "Done"
+    fi
 done
