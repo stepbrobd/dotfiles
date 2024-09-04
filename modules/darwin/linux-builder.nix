7fd147397f50151ab723c93b60d88fb414bb1bfe
Yifei Sun <ysun@hey.com>
2024-09-04 15:48:51 -0400

    modules: use x86_64 builder package

--- modules/darwin/linux-builder.nix
+++ modules/darwin/linux-builder.nix
@@ -1,7 +1,12 @@
+{ pkgs, ... }:
+
 {
   nix.linux-builder = {
     enable = true;
+    ephemeral = true;
+
     maxJobs = 2;
+    package = pkgs.darwin.linux-builder-x86_64;
     systems = [ "x86_64-linux" ];
   };
 }
