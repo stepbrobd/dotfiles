b669e1af08a84c39e56c92a8037bb42e784c195d
Yifei Sun <ysun@hey.com>
2024-10-09 16:44:06 -0400

    packages: fix eval warning

--- packages/nixvim/plugins/lsp.nix
+++ packages/nixvim/plugins/lsp.nix
@@ -17,7 +17,7 @@
 
   # Docker
   plugins.lsp.servers.dockerls.enable = true;
-  plugins.lsp.servers.docker-compose-language-service.enable = true;
+  plugins.lsp.servers.docker_compose_language_service.enable = true;
 
   # Go
   plugins.lsp.servers.gopls.enable = true;
@@ -26,7 +26,7 @@
   plugins.lsp.servers.cssls.enable = true; # CSS
   plugins.lsp.servers.html.enable = true; # HTML
   plugins.lsp.servers.tailwindcss.enable = true; # TailwindCSS
-  plugins.lsp.servers.ts-ls.enable = true; # TS/JS
+  plugins.lsp.servers.ts_ls.enable = true; # TS/JS
 
   # JSON
   plugins.lsp.servers.jsonls.enable = true;
@@ -35,25 +35,25 @@
   plugins.lsp.servers.marksman.enable = true;
 
   # Nix
-  plugins.lsp.servers.nil-ls.enable = true;
+  plugins.lsp.servers.nil_ls.enable = true;
   plugins.nix.enable = true;
 
   # Python
   plugins.lsp.servers.ruff.enable = true;
-  plugins.lsp.servers.ruff-lsp.enable = true;
+  plugins.lsp.servers.ruff_lsp.enable = true;
 
   # Shell
   plugins.lsp.servers.bashls.enable = true;
   plugins.lsp.servers.nushell.enable = true;
 
   # Spelling
-  plugins.lsp.servers.typos-lsp.enable = true;
+  plugins.lsp.servers.typos_lsp.enable = true;
 
   # TeX
   plugins.lsp.servers.ltex.enable = true;
 
   # Typst
-  plugins.lsp.servers.typst-lsp.enable = true;
+  plugins.lsp.servers.typst_lsp.enable = true;
   plugins.typst-vim = {
     enable = true;
     settings.pdf_viewer = "zathura";
