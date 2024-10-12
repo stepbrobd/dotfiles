a9eadd9571e74ee826ce1832e3f4d9eace616479
Yifei Sun <ysun@hey.com>
2024-10-12 19:44:07 -0400

    hosts: change domain

--- hosts/server/walberla/keycloak.nix
+++ hosts/server/walberla/keycloak.nix
@@ -7,7 +7,7 @@
       http-enabled = true;
       http-host = "127.0.0.1";
       http-port = 8080;
-      hostname = "id.ysun.co";
+      hostname = "auth.ysun.co";
       proxy-headers = "xforwarded";
       cache = "local";
     };
