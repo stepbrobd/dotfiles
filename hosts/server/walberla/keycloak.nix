763e41c93e500446c8612930d1088d1497321816
Yifei Sun <ysun@hey.com>
2024-10-12 16:35:17 -0400

    hosts: keycloak proxy

--- hosts/server/walberla/keycloak.nix
+++ hosts/server/walberla/keycloak.nix
@@ -4,9 +4,11 @@
   services.keycloak = {
     enable = true;
     settings = {
+      http-enabled = true;
       http-host = "127.0.0.1";
       http-port = 8080;
       hostname = "id.ysun.co";
+      proxy-headers = "xforwarded";
       cache = "local";
     };
     database.passwordFile = toString (pkgs.writeText "password" "keycloak");
