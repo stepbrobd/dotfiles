bf10be46fe8122112f9b34fd17f129cb38833491
Yifei Sun <ysun@hey.com>
2024-10-21 15:14:29 -0400

    modules: kanidm uses acme certs

--- hosts/server/walberla/kanidm.nix
+++ hosts/server/walberla/kanidm.nix
@@ -1,30 +1,26 @@
 { config, pkgs, ... }:
 
-let
-  domain = "auth.ysun.co";
-  caddyDir = "/var/lib/caddy/.local/share/caddy/certificates/acme-v02.api.letsencrypt.org-directory";
-in
 {
   services.kanidm = {
     enableServer = true;
 
-    serverSettings = {
-      inherit domain;
-      origin = "https://${domain}";
-      ldapbindaddress = "0.0.0.0:636";
-      bindaddress = "0.0.0.0:8080";
-      trust_x_forward_for = true;
-      tls_key = "${caddyDir}/${domain}/${domain}.key";
-      tls_chain = "${caddyDir}/${domain}/${domain}.crt";
-    };
+    serverSettings =
+      let
+        inherit (config.security.acme.certs."auth.ysun.co") directory;
+      in
+      {
+        domain = "auth.ysun.co";
+        origin = "https://auth.ysun.co";
+        ldapbindaddress = "0.0.0.0:636";
+        bindaddress = "0.0.0.0:8080";
+        trust_x_forward_for = true;
+        tls_key = "${directory}/privkey.pem";
+        tls_chain = "${directory}/fullchain.pem";
+      };
   };
 
-  # kanidm have read access to cadddy certificates
-  systemd.services.kanidm.preStart = ''
-    ${pkgs.acl}/bin/setfacl -m user:kanidm:r ${config.services.kanidm.serverSettings.tls_key}
-    ${pkgs.acl}/bin/setfacl -m user:kanidm:r ${config.services.kanidm.serverSettings.tls_chain}
-
-    ${pkgs.acl}/bin/setfacl -m group:kanidm:r ${config.services.kanidm.serverSettings.tls_key}
-    ${pkgs.acl}/bin/setfacl -m group:kanidm:r ${config.services.kanidm.serverSettings.tls_chain}
-  '';
+  security.acme.certs."auth.ysun.co" = {
+    postRun = "systemctl restart kanidm.service";
+    group = "kanidm";
+  };
 }
