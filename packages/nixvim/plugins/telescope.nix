b87c7964171c684b71e430c290d0af49250ba310
Yifei Sun <ysun@hey.com>
2024-11-13 18:46:57 -0500

    packages: add telescope keymapping

--- packages/nixvim/plugins/telescope.nix
+++ packages/nixvim/plugins/telescope.nix
@@ -1,5 +1,24 @@
 {
-  plugins.telescope = {
-    enable = true;
-  };
+  plugins.telescope.enable = true;
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>ff";
+      action = "<cmd>:Telescope find_files<cr>";
+      options = {
+        silent = true;
+        desc = "Telescope find files";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fg";
+      action = "<cmd>:Telescope live_grep<cr>";
+      options = {
+        silent = true;
+        desc = "Telescope live grep";
+      };
+    }
+  ];
 }
