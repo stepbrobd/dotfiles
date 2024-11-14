a92fa12dd12eb5492f249eb58b87ca104d32c124
Yifei Sun <ysun@hey.com>
2024-11-13 20:43:50 -0500

    packages: refactor nixvim

--- packages/nixvim/default.nix
+++ packages/nixvim/default.nix
@@ -7,8 +7,12 @@
 
 let
   nixvim = inputs.nixvim.legacyPackages."${stdenv.system}";
-  inherit (lib) attrNames map readDir;
+  inherit (lib) attrNames filter map readDir;
 in
 lib.makeOverridable nixvim.makeNixvimWithModule {
-  module.imports = map (x: ./plugins/. + "/${x}") (attrNames (readDir ./plugins));
+  module.imports = map
+    (f: ./${f})
+    (filter
+      (f: f != "default.nix")
+      (attrNames (readDir ./.)));
 }
