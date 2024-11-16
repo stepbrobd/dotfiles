439744737a7c3916fdc63c5248f1c9b8debb3a04
Yifei Sun <ysun@hey.com>
2024-11-15 22:15:07 -0500

    packages: disable alpha and nvimtree on lualine

--- packages/nixvim/lualine.nix
+++ packages/nixvim/lualine.nix
@@ -1,11 +1,14 @@
 {
   plugins.lualine = {
     enable = true;
+
     settings = {
       options = {
         theme = "nord";
         icons_enabled = true;
+        disabled_filetypes.statusline = [ "alpha" "NvimTree" ];
       };
+
       sections = {
         lualine_a = [ "mode" ];
         lualine_b = [ "branch" ];
