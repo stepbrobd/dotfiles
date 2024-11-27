40577776a04bcf5b059b753d25226aa68b878655
Yifei Sun <ysun@hey.com>
2024-11-27 17:02:07 -0500

    modules: darwin builder x86_64-linux???

--- modules/darwin/linux-builder.nix
+++ modules/darwin/linux-builder.nix
@@ -1,8 +1,22 @@
+{ pkgs, ... }:
+
 {
+  nix.settings.trusted-users = [ "@admin" ];
   nix.linux-builder = {
     enable = true;
     ephemeral = true;
     maxJobs = 8;
+
+    package = pkgs.darwin.linux-builder-x86_64;
     systems = [ "x86_64-linux" ];
+    config = {
+      virtualisation = {
+        darwin-builder = {
+          diskSize = 25 * 1024;
+          memorySize = 8 * 1024;
+        };
+        cores = 8;
+      };
+    };
   };
 }
