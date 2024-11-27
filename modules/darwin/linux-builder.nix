1c8a8e7ba55e92dce21d38f50bfe31ac7b416dc0
Yifei Sun <ysun@hey.com>
2024-11-27 16:55:08 -0500

    modules: darwin linux builder

--- modules/darwin/linux-builder.nix
+++ modules/darwin/linux-builder.nix
@@ -4,9 +4,6 @@
   nix.linux-builder = {
     enable = true;
     ephemeral = true;
-
-    maxJobs = 2;
-    package = pkgs.darwin.linux-builder-x86_64;
-    systems = [ "x86_64-linux" ];
+    maxJobs = 8;
   };
 }
