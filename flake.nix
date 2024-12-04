4f6d17e08bd8dbf94e9bd3f01c7487a6fd350151
Yifei Sun <ysun@hey.com>
2024-12-04 13:51:57 -0500

    flake: update inputs

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs/master";
     # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/e8c9fa5bc83aeaa2c6382a95bb56a94598cf2354";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # a
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
