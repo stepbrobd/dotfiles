e1ddee29ccecedcde1317e0e040a98e4cb07931d
Yifei Sun <ysun@hey.com>
2024-12-27 20:20:57 -0500

    modules: remove mymind

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -87,7 +87,6 @@
       # safari
       "AdGuard" = 1440147259;
       "Kagi" = 1622835804;
-      "MyMind" = 1532801185;
       "Noir" = 1592917505;
       "Vimari" = 1480933944;
       # microsoft
