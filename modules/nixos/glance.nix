2432c77ec278ddae0d86231c2def7b2d77ec61d6
Yifei Sun <ysun@hey.com>
2024-12-28 22:02:27 -0500

    modules: add bookmarks

--- modules/nixos/glance.nix
+++ modules/nixos/glance.nix
@@ -59,6 +59,41 @@ in
                   autofocus = true;
                   search-engine = "https://kagi.com/search?q={QUERY}";
                 }
+                {
+                  type = "bookmarks";
+                  groups = [
+                    {
+                      title = "Bookmarks";
+                      links = [
+                        { title = "Home"; url = "https://home.ysun.co/"; icon = "si:homepage"; same-tab = true; }
+                        { title = "Kagi"; url = "https://kagi.com/"; icon = "si:kagi"; same-tab = true; }
+                        { title = "GoLink"; url = "https://go.tail650e82.ts.net/"; icon = "si:shortcut"; same-tab = true; }
+                        { title = "Hey"; url = "https://app.hey.com/imbox"; icon = "si:hey"; same-tab = true; }
+                        { title = "GitHub"; url = "https://github.com/"; icon = "si:github"; same-tab = true; }
+                        { title = "SourceHut"; url = "https://git.sr.ht/~stepbrobd/"; icon = "si:sourcehut"; same-tab = true; }
+                        { title = "NextDNS"; url = "https://my.nextdns.io/"; icon = "si:nextdns"; same-tab = true; }
+                        { title = "Plausible"; url = "https://stats.ysun.co/ysun.co"; icon = "si:plausibleanalytics"; same-tab = true; }
+                        { title = "Cloudflare"; url = "https://dash.cloudflare.com/6ff6fca6d9ffe9c77dd15a9095076b3b"; icon = "si:cloudflare"; same-tab = true; }
+                        { title = "Tailscale"; url = "https://login.tailscale.com/admin/machines"; icon = "si:tailscale"; same-tab = true; }
+                        { title = "AWS"; url = "https://eu-central-2.console.aws.amazon.com/ec2/home?region=eu-central-2#Instances:"; icon = "si:amazonwebservices"; same-tab = true; }
+                        { title = "Lemmy"; url = "https://phtn.app/"; icon = "si:lemmy"; same-tab = true; }
+                        { title = "NixOS"; url = "https://discourse.nixos.org/"; icon = "si:nixos"; same-tab = true; }
+                        { title = "Framework"; url = "https://community.frame.work/"; icon = "si:framework"; same-tab = true; }
+                        { title = "OCaml"; url = "https://discuss.ocaml.org/"; icon = "si:ocaml"; same-tab = true; }
+                        { title = "Churning"; url = "https://www.uscardforum.com/"; icon = "si:cashapp"; same-tab = true; }
+                        { title = "Purelymail"; url = "https://purelymail.com/manage/domains"; icon = "si:maildotru"; same-tab = true; }
+                        { title = "Tildes"; url = "https://tildes.net/"; icon = "si:bookmeter"; same-tab = true; }
+                        { title = "Lobsters"; url = "https://lobste.rs/active"; icon = "si:lobsters"; same-tab = true; }
+                        { title = "Hacker News"; url = "https://hn.algolia.com/"; icon = "si:ycombinator"; same-tab = true; }
+                        { title = "LinkedIn"; url = "https://www.linkedin.com/"; icon = "si:linkerd"; same-tab = true; }
+                        { title = "Timetable"; url = "https://animeschedule.net/"; icon = "si:myanimelist"; same-tab = true; }
+                        { title = "Netflix"; url = "https://www.netflix.com/browse"; icon = "si:netflix"; same-tab = true; }
+                        { title = "Disney+"; url = "https://www.disneyplus.com/home"; icon = "si:airplayvideo"; same-tab = true; }
+                      ];
+                    }
+                  ];
+                }
+
               ];
             }
             {
