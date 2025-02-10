67e501749696688a8482ce6c771b22ec1321ef83
Yifei Sun <ysun@hey.com>
2025-02-09 22:05:32 -0500

    packages: add dolmen_lsp

---
+++ packages/dolmen_lsp/default.nix
@@ -0,0 +1,23 @@
+{  ocamlPackages }:
+
+ocamlPackages.buildDunePackage {
+  pname = "dolmen_lsp";
+  inherit (ocamlPackages.dolmen) version src;
+
+  dontDetectOcamlConflicts = true;
+  minimalOCamlVersion = "4.02";
+
+  patches = [ ./linol-lwt-6.patch ];
+
+  buildInputs = with ocamlPackages; [
+    dolmen
+    dolmen_loop
+    dolmen_type
+    linol
+    linol-lwt
+    logs
+    lsp
+  ];
+
+  meta.mainProgram = "dolmenls";
+}
