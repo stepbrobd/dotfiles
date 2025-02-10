884102721bc77173b5317e0d2e8cd56bf4ef7c79
Yifei Sun <ysun@hey.com>
2025-02-09 23:11:37 -0500

    packages: add note

--- packages/dolmen_lsp/default.nix
+++ packages/dolmen_lsp/default.nix
@@ -1,10 +1,11 @@
+# remove after nixpkgs#380805 is merged
+
 { ocamlPackages }:
 
 ocamlPackages.buildDunePackage {
   pname = "dolmen_lsp";
   inherit (ocamlPackages.dolmen) version src;
 
-  dontDetectOcamlConflicts = true;
   minimalOCamlVersion = "4.02";
 
   patches = [ ./linol-lwt-6.patch ];
@@ -16,7 +17,6 @@ ocamlPackages.buildDunePackage {
     linol
     linol-lwt
     logs
-    lsp
   ];
 
   meta.mainProgram = "dolmenls";
