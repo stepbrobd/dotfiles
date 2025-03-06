06436557a70dd6244376f43b1af9c40c2fe653ed
Yifei Sun <ysun@hey.com>
2025-03-06 17:32:32 -0500

    modules: disable tailscale check on darwin

---
+++ packages/tailscale/default.nix
@@ -0,0 +1,10 @@
+{ stdenv
+, pkgs
+, pkgsPrev ? pkgs # `pkgsPrev` only provided in overlays
+}:
+
+if stdenv.isDarwin
+then
+  pkgsPrev.tailscale.overrideAttrs
+    (_: { doCheck = false; }) # remove after nixpkgs #387340 closes
+else pkgsPrev.tailscle
