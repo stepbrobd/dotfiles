d4d92615d1ca6345cda37dc586e7f4705f83984a
Yifei Sun <ysun@hey.com>
2025-03-06 10:30:20 -0500

    hosts: goffle disallow root login

--- hosts/server/goffle/default.nix
+++ hosts/server/goffle/default.nix
@@ -5,8 +5,6 @@
     # ./as10779.nix
   ];
 
-  users.users.root.openssh.authorizedKeys.keys = [ ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw'' ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBVZ9mzYNxccuh3uQR7Hly4KjhbRh4s6UlGQe2GjMtIC'' ];
-
   networking = {
     # head -c4 /dev/urandom | od -A none -t x4
     hostId = "a276fa18";
