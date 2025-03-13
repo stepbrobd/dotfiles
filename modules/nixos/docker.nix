fbe32bbf18f99f4050bd74d7a5eae7e462be03c3
Yifei Sun <ysun@hey.com>
2025-03-13 12:17:45 -0400

    modules: disable docker

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -1,9 +1,16 @@
+{ lib, ... }:
+
 {
-  virtualisation.docker = {
-    enable = true;
-    rootless = {
-      enable = true;
-      setSocketVariable = true;
+  # use podman
+  virtualisation.docker.enable = lib.mkForce false;
+
+  virtualisation.oci-containers.backend = "podman";
+  virtualisation.containers.enable = false;
+  virtualisation = {
+    podman = {
+      enable = false;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
     };
   };
 }
