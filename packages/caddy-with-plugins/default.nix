f7738ea62e50b557eb1e68942d711d68e2e68e69
Yifei Sun <ysun@hey.com>
2025-03-16 10:51:39 -0400

    flake: update inputs

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 
 pkgs.caddy.withPlugins {
-  hash = "sha256-V9x+AQzeOh3YH/ydyHzd5vVTC+38pNemk1XIJ9IBl0I=";
+  hash = "sha256-wSgBOXa+OnZkwIKebSmvOizxZee5Zg/g3Di7fEO4FsI=";
   plugins = [
     "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
     "github.com/caddy-dns/cloudflare@v0.0.0-20250228175314-1fb64108d4de"
