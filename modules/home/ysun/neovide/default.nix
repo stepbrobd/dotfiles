9cbbb069b19dc023563276c3db388fe5b2f74f57
Yifei Sun <ysun@hey.com>
2025-03-25 14:55:43 -0400

    modules: neovide should use the alacritty font settings

--- modules/home/ysun/neovide/default.nix
+++ modules/home/ysun/neovide/default.nix
@@ -1,7 +1,11 @@
+{ config, ... }:
+
 {
   programs.neovide = {
     enable = true;
     settings = {
+      inherit (config.programs.alacritty.settings) font;
+
       theme = "auto";
       frame = "none";
       title-hidden = true;
