2bc0e615cc77ae9b1f44441f494380913154e1bf
Yifei Sun <ysun@hey.com>
2025-03-25 16:05:15 -0400

    modules: use patched neovide

--- modules/home/ysun/neovide/default.nix
+++ modules/home/ysun/neovide/default.nix
@@ -1,8 +1,13 @@
-{ config, ... }:
+{ inputs, ... }:
+
+{ config, pkgs, ... }:
 
 {
   programs.neovide = {
     enable = true;
+
+    package = inputs.self.packages.${pkgs.stdenv.system}.neovide;
+
     settings = {
       inherit (config.programs.alacritty.settings) font;
 
