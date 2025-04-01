24a19dca091578b3c33c3a78806dcb566dbb9126
Yifei Sun <ysun@hey.com>
2025-04-01 14:09:38 -0400

    hosts: butte enable bgp

--- hosts/server/butte/as10779.nix
+++ hosts/server/butte/as10779.nix
@@ -7,9 +7,9 @@ in
   sops.secrets.bgp = {
     sopsFile = ./secrets.yaml;
     mode = "440";
-    # owner = config.systemd.services.bird.serviceConfig.User;
-    # group = config.systemd.services.bird.serviceConfig.Group;
-    # reloadUnits = [ config.systemd.services.bird.name ];
+    owner = config.systemd.services.bird.serviceConfig.User;
+    group = config.systemd.services.bird.serviceConfig.Group;
+    reloadUnits = [ config.systemd.services.bird.name ];
   };
 
   services.as10779 = {
@@ -19,40 +19,60 @@ in
       hostname = config.networking.hostName;
       interface = {
         local = "dummy0";
-        primary = "enp3s0";
+        primary = "eth0";
       };
-      ipv4.address = "23.161.104.131/32";
-      ipv6.address = "2620:be:a000::23:161:104:131/128";
+      ipv4.address = "23.161.104.132/32";
+      ipv6.address = "2620:be:a000::23:161:104:132/128";
     };
 
     router = {
-      id = lib.blueprint.hosts.diablo.ipv4;
-      exit = false;
+      id = lib.blueprint.hosts.butte.ipv4;
       secret = config.sops.secrets.bgp.path;
-      source = { inherit (lib.blueprint.hosts.diablo) ipv4 ipv6; };
+      source = { inherit (lib.blueprint.hosts.butte) ipv4 ipv6; };
       static = {
-        ipv4.routes = [{ prefix = "23.161.104.0/24"; option = "reject"; }];
+        ipv4.routes = [
+          { prefix = "23.161.104.0/24"; option = "reject"; }
+          { prefix = "44.32.189.0/24"; option = "reject"; }
+        ];
         ipv6.routes = [{ prefix = "2620:be:a000::/48"; option = "reject"; }];
       };
       sessions = [
-        # {
-        #   name = "misaka";
-        #   password = "PASS_AS917";
-        #   type = { ipv4 = "direct"; ipv6 = "direct"; };
-        #   neighbor = {
-        #     asn = 64515;
-        #     ipv4 = "";
-        #     ipv6 = "";
-        #   };
-        #   import = {
-        #     ipv4 = "import filter ${cfg.router.rpki.ipv4.filter};";
-        #     ipv6 = "import filter ${cfg.router.rpki.ipv6.filter};";
-        #   };
-        #   export = {
-        #     ipv4 = ''export where proto = "${cfg.router.static.ipv4.name}";'';
-        #     ipv6 = ''export where proto = "${cfg.router.static.ipv6.name}";'';
-        #   };
-        # }
+        {
+          name = "virtua1";
+          password = "PASS_AS35661";
+          type = { ipv4 = "multihop"; ipv6 = "multihop"; };
+          neighbor = {
+            asn = 35661;
+            ipv4 = "172.16.0.121";
+            ipv6 = "2a0d:e680:0::b:1";
+          };
+          import = {
+            ipv4 = "import filter ${cfg.router.rpki.ipv4.filter};";
+            ipv6 = "import filter ${cfg.router.rpki.ipv6.filter};";
+          };
+          export = {
+            ipv4 = ''export where proto = "${cfg.router.static.ipv4.name}";'';
+            ipv6 = ''export where proto = "${cfg.router.static.ipv6.name}";'';
+          };
+        }
+        {
+          name = "virtua2";
+          password = "PASS_AS35661";
+          type = { ipv4 = "multihop"; ipv6 = "multihop"; };
+          neighbor = {
+            asn = 35661;
+            ipv4 = "172.16.0.122";
+            ipv6 = "2a0d:e680:0::b:2";
+          };
+          import = {
+            ipv4 = "import filter ${cfg.router.rpki.ipv4.filter};";
+            ipv6 = "import filter ${cfg.router.rpki.ipv6.filter};";
+          };
+          export = {
+            ipv4 = ''export where proto = "${cfg.router.static.ipv4.name}";'';
+            ipv6 = ''export where proto = "${cfg.router.static.ipv6.name}";'';
+          };
+        }
         # {
         #   name = "bgptools";
         #   password = null;
