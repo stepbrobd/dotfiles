e90f42f1798557b7be793c55954e4c3970f76199
Yifei Sun <ysun@hey.com>
2025-04-03 21:40:43 -0400

    hosts: try vxlan

--- hosts/server/kongo/as10779.nix
+++ hosts/server/kongo/as10779.nix
@@ -12,6 +12,26 @@ in
     reloadUnits = [ config.systemd.services.bird.name ];
   };
 
+  systemd.network = {
+    networks."45-vx0" = {
+      name = "vx0";
+      address = [ "100.66.33.17/22" "2a0e:8f01:1000:9::111/64" ];
+    };
+    netdevs."45-vx0" = {
+      netdevConfig = {
+        Name = "vx0";
+        Kind = "vxlan";
+      };
+      vxlanConfig = {
+        VNI = 9558;
+        Local = lib.blueprint.hosts.kongo.ipv4;
+        Remote = "156.231.102.211";
+        DestinationPort = 4789;
+        Independent = true;
+      };
+    };
+  };
+
   services.as10779 = {
     enable = true;
 
