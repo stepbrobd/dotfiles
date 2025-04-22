0ef4f8e152f3f78b1973de6041fdb50831470fcd
Yifei Sun <ysun@hey.com>
2025-04-22 19:40:23 -0400

    packages: caddy update

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -1,9 +1,9 @@
 { pkgs }:
 
 pkgs.caddy.withPlugins {
-  hash = "sha256-wSgBOXa+OnZkwIKebSmvOizxZee5Zg/g3Di7fEO4FsI=";
+  hash = "sha256-Rnroq/pv83PUWs8x2Ag3ZjsxxVmFzHc++5H84iwlYfU=";
   plugins = [
     "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
-    "github.com/caddy-dns/cloudflare@v0.0.0-20250228175314-1fb64108d4de"
+    "github.com/caddy-dns/cloudflare@v0.2.2-0.20250420134112-006ebb07b349"
   ];
 }
