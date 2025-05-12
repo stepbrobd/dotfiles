1a49de716ea01e6837e92302ac5b580bc6bd3e89
Yifei Sun <ysun@hey.com>
2025-05-12 19:31:48 -0400

    modules: fix neomutt

--- modules/home/ysun/neomutt.nix
+++ modules/home/ysun/neomutt.nix
@@ -1,13 +1,100 @@
-{ config, ... }:
+{ lib, ... }:
+
+{ config, pkgs, ... }:
 
 {
+  accounts.email = {
+    maildirBasePath = "${config.xdg.dataHome}/mail";
+
+    accounts =
+      let
+        realName = "Yifei Sun";
+
+        mbsync.enable = true;
+        msmtp.enable = true;
+        notmuch = {
+          enable = true;
+          neomutt = {
+            enable = true;
+            virtualMailboxes = [ ];
+          };
+        };
+
+        neomutt = {
+          enable = true;
+          mailboxType = "imap";
+        };
+
+        mkpass = key: "${lib.getExe' pkgs.toybox "cat"} ${config.sops.defaultSymlinkPath}/${key}";
+      in
+      {
+        goo = rec {
+          inherit realName mbsync msmtp notmuch neomutt;
+          address = "ysun@goo.jp";
+          userName = address;
+          passwordCommand = mkpass "mail/goo/pass";
+          imap = {
+            host = "imap.mail.goo.jp";
+            port = 993;
+          };
+          smtp = {
+            host = "smtp.mail.goo.jp";
+            port = 465;
+          };
+        };
+
+        softbank = rec {
+          inherit realName mbsync msmtp notmuch neomutt;
+          address = "ysun@i.softbank.jp";
+          userName = address;
+          passwordCommand = mkpass "mail/softbank/pass";
+          imap = {
+            host = "imap.softbank.jp";
+            port = 993;
+          };
+          smtp = {
+            host = "smtp.softbank.jp";
+            port = 465;
+          };
+        };
+
+        stepbrobd = rec {
+          primary = true;
+          inherit realName mbsync msmtp notmuch neomutt;
+          address = "ysun@stepbrobd.com";
+          userName = address;
+          passwordCommand = mkpass "mail/stepbrobd/pass";
+          imap = {
+            host = "imap.purelymail.com";
+            port = 993;
+          };
+          smtp = {
+            host = "smtp.purelymail.com";
+            port = 465;
+          };
+        };
+      };
+  };
+
+  sops.secrets."mail/goo/pass" = { };
+  sops.secrets."mail/softbank/pass" = { };
+  sops.secrets."mail/stepbrobd/pass" = { };
+
+  programs.mbsync.enable = true;
+  programs.msmtp.enable = true;
+  programs.notmuch.enable = true;
+
   programs.neomutt = {
-    enable = false;
+    enable = true;
     vimKeys = true;
 
-    extraConfig = ''
-      set folder=${config.xdg.dataHome}/neomutt
+    sidebar = {
+      enable = true;
+      shortPath = true;
+      width = 25;
+    };
 
+    extraConfig = ''
       color normal		default default
       color index		brightblue default ~N
       color index		red default ~F
