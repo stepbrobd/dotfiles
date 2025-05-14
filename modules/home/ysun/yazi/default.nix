915ed88860ff723ae1da47555f4839545838d275
Yifei Sun <ysun@hey.com>
2025-05-14 17:13:39 -0400

    modules: yazi nord theme

--- modules/home/ysun/yazi/default.nix
+++ modules/home/ysun/yazi/default.nix
@@ -1,12 +1,30 @@
+{ inputs, ... }:
+
 { pkgs, ... }:
 
+let
+  nord = inputs.nord.packages.${pkgs.stdenv.system}.default;
+in
 {
   programs.yazi = {
     enable = true;
 
     plugins = with pkgs.yaziPlugins; {
-      inherit full-border git yatline;
+      inherit full-border git nord yatline;
     };
+
+    flavors = { inherit nord; };
+
+    theme.flavor = {
+      light = "nord";
+      dark = "nord";
+    };
+
+    # pkgs.yaziPlugins.git
+    settings.plugin.prepend_fetchers = [
+      { id = "git"; name = "*"; run = "git"; }
+      { id = "git"; name = "*/"; run = "git"; }
+    ];
 
     initLua = /* lua */ ''
       -- pkgs.yaziPlugins.full-border
@@ -17,6 +35,9 @@
 
       -- pkgs.yaziPlugins.yatline
       require("yatline"):setup({
+        -- pkgs.yaziPlugins.nord
+        theme = require("nord"):setup(),
+
         header_line = {
           left = {
             section_a = {
@@ -60,277 +81,7 @@
             },
           },
         },
-
-        theme = {
-          section_separator = { open = "", close = "" },
-          part_separator = { open = "", close = "" },
-          inverse_separator = { open = "", close = "" },
-
-          style_a = {
-                  fg = "black",
-                  bg_mode = {
-                          normal = "#81A1C1",
-                          select = "#88C0D0",
-                          un_set = "#D08770"
-                  }
-          },
-          style_b = { bg = "#4C566A", fg = "#E5E9F0" },
-          style_c = { bg = "#3B4252", fg = "#81A1C1" },
-
-          permissions_t_fg = "#A3BE8C",
-          permissions_r_fg = "#EBCB8B",
-          permissions_w_fg = "#BF616A",
-          permissions_x_fg = "#88C0D0",
-          permissions_s_fg = "#E5E9F0",
-
-          selected = { icon = "󰻭", fg = "#EBCB8B" },
-          copied = { icon = "", fg = "#A3BE8C" },
-          cut = { icon = "", fg = "#BF616A" },
-          files = { icon = "", fg = "#5E81AC" },
-          filtereds = { icon = "", fg = "#B48EAD" },
-
-          total = { icon = "󰮍", fg = "#EBCB8B" },
-          succ = { icon = "", fg = "#A3BE8C" },
-          fail = { icon = "", fg = "#BF616A" },
-          found = { icon = "󰮕", fg = "#5E81AC" },
-          processed = { icon = "󰐍", fg = "#A3BE8C" },
-        },
       })
     '';
-
-    # pkgs.yaziPlugins.git
-    settings.plugin.prepend_fetchers = [
-      { id = "git"; name = "*"; run = "git"; }
-      { id = "git"; name = "*/"; run = "git"; }
-    ];
-
-    theme = {
-      cmp = {
-        border = {
-          fg = "#81A1C1";
-        };
-      };
-      filetype = {
-        rules = [
-          {
-            fg = "#88C0D0";
-            mime = "image/*";
-          }
-          {
-            fg = "#EBCB8B";
-            mime = "{audio,video}/*";
-          }
-          {
-            fg = "#B48EAD";
-            mime = "application/{zip,rar,7z*,tar,gzip,xz,zstd,bzip*,lzma,compress,archive,cpio,arj,xar,ms-cab*}";
-          }
-          {
-            fg = "#A3BE8C";
-            mime = "application/{pdf,doc,rtf}";
-          }
-          {
-            fg = "#E5E9F0";
-            name = "*";
-          }
-          {
-            fg = "#81A1C1";
-            name = "*/";
-          }
-        ];
-      };
-      help = {
-        footer = {
-          bg = "#E5E9F0";
-          fg = "#3B4252";
-        };
-        hovered = {
-          bold = true;
-          reversed = true;
-        };
-        on = {
-          fg = "#88C0D0";
-        };
-        run = {
-          fg = "#B48EAD";
-        };
-      };
-      input = {
-        border = {
-          fg = "#81A1C1";
-        };
-        selected = {
-          reversed = true;
-        };
-        title = { };
-        value = { };
-      };
-      manager = {
-        border_style = {
-          fg = "#7B88A1";
-        };
-        border_symbol = "│";
-        count_copied = {
-          bg = "#A3BE8C";
-          fg = "#2E3440";
-        };
-        count_cut = {
-          bg = "#BF616A";
-          fg = "#2E3440";
-        };
-        count_selected = {
-          bg = "#EBCB8B";
-          fg = "#2E3440";
-        };
-        cwd = {
-          fg = "#88C0D0";
-        };
-        find_keyword = {
-          bold = true;
-          fg = "#EBCB8B";
-          italic = true;
-          underline = true;
-        };
-        find_position = {
-          bg = "reset";
-          bold = true;
-          fg = "#B48EAD";
-          italic = true;
-        };
-        hovered = {
-          reversed = true;
-        };
-        marker_copied = {
-          bg = "#A3BE8C";
-          fg = "#A3BE8C";
-        };
-        marker_cut = {
-          bg = "#BF616A";
-          fg = "#BF616A";
-        };
-        marker_marked = {
-          bg = "#88C0D0";
-          fg = "#88C0D0";
-        };
-        marker_selected = {
-          bg = "#EBCB8B";
-          fg = "#EBCB8B";
-        };
-        preview_hovered = {
-          underline = true;
-        };
-        tab_active = {
-          reversed = true;
-        };
-        tab_inactive = { };
-        tab_width = 1;
-      };
-      mode = {
-        normal_alt = {
-          bg = "#3B4252";
-          fg = "#81A1C1";
-        };
-        normal_main = {
-          bg = "#81A1C1";
-          bold = true;
-          fg = "#2E3440";
-        };
-        select_alt = {
-          bg = "#3B4252";
-          fg = "#88C0D0";
-        };
-        select_main = {
-          bg = "#88C0D0";
-          bold = true;
-          fg = "#2E3440";
-        };
-        unset_alt = {
-          bg = "#3B4252";
-          fg = "#D08770";
-        };
-        unset_main = {
-          bg = "#D08770";
-          bold = true;
-          fg = "#2E3440";
-        };
-      };
-      notify = {
-        title_error = {
-          fg = "#BF616A";
-        };
-        title_info = {
-          fg = "#A3BE8C";
-        };
-        title_warn = {
-          fg = "#EBCB8B";
-        };
-      };
-      pick = {
-        active = {
-          bold = true;
-          fg = "#B48EAD";
-        };
-        border = {
-          fg = "#81A1C1";
-        };
-        inactive = { };
-      };
-      status = {
-        perm_exec = {
-          fg = "#A3BE8C";
-        };
-        perm_read = {
-          fg = "#EBCB8B";
-        };
-        perm_sep = {
-          fg = "#7B88A1";
-        };
-        perm_type = {
-          fg = "#81A1C1";
-        };
-        perm_write = {
-          fg = "#BF616A";
-        };
-        progress_error = {
-          bg = "#434C5E";
-          fg = "#BF616A";
-        };
-        progress_label = {
-          bold = true;
-          fg = "#ffffff";
-        };
-        progress_normal = {
-          bg = "#434C5E";
-          fg = "#81A1C1";
-        };
-      };
-      tasks = {
-        border = {
-          fg = "#81A1C1";
-        };
-        hovered = {
-          fg = "#B48EAD";
-          underline = true;
-        };
-        title = { };
-      };
-      which = {
-        cand = {
-          fg = "#88C0D0";
-        };
-        desc = {
-          fg = "#B48EAD";
-        };
-        mask = {
-          bg = "#3B4252";
-        };
-        rest = {
-          fg = "#8FBCBB";
-        };
-        separator = "  ";
-        separator_style = {
-          fg = "#4C566A";
-        };
-      };
-    };
   };
 }
