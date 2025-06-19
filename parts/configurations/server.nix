c9126d48739477a3d97c4cb11008a7cccceb4769
Yifei Sun <ysun@hey.com>
2025-06-19 23:57:13 +0200

    hosts: use pinned nixos-raspberrypi (bluetooth still broken?)

--- parts/configurations/server.nix
+++ parts/configurations/server.nix
@@ -2,68 +2,59 @@
 
 let
   inherit (lib) mkColmena;
-  hosts = [
-    "butte" # Virtua Cloud, 1 vCPU, 1GB RAM, 20GB Storage
-    "halti" # Garnix.io Hosting, test server
-    "highline" # Neptune Networks, 1 vCPU, 1GB RAM, 10GB Storage
-    "kongo" # Vultr, 1 vCPU, 1GB RAM, 25GB Storage
-    "lagern" # AWS, T3.Large, 25GB Storage
-    "odake" # SSDNodes NRT Performance, 8 vCPU, 32GB RAM, 640GB Storage
-    "toompea" # V.PS Pro Tallinn, 4 vCPU, 4GB RAM, 40GB Storage
-    "walberla" # Hetzner Cloud CX32, 4 vCPU, 8GB RAM, 80GB Storage
-  ];
 
-  colmena = (mkColmena rec {
-    inherit inputs hosts stateVersion;
-    os = "nixos";
-    platform = "x86_64-linux";
-    users = { ysun = with inputs.self; [ hmModules.ysun.minimal ]; };
-    modules = with inputs; [
-      self.nixosModules.acme
-      self.nixosModules.as10779
-      self.nixosModules.attic
-      self.nixosModules.caddy
-      self.nixosModules.calibre
-      self.nixosModules.common
-      self.nixosModules.desktop
-      self.nixosModules.glance
-      self.nixosModules.golink
-      self.nixosModules.grafana
-      self.nixosModules.maxmind
-      self.nixosModules.minimal
-      self.nixosModules.passwordless
-      self.nixosModules.plausible
-      self.nixosModules.prometheus
-      self.nixosModules.rebuild
-      self.nixosModules.server
-      self.nixosModules.uptime
-      srvos.nixosModules.common
-      srvos.nixosModules.server
-    ];
-    nixpkgs = (getSystem platform).allModuleArgs.pkgs;
-    specialArgs = { inherit inputs lib; };
-  }) // (mkColmena rec {
-    inherit inputs stateVersion;
-    hosts = [ "isere" ]; # Raspberry Pi 4, 8GB RAM, 500GB Storage
-    os = "nixos";
-    platform = "aarch64-linux";
-    users = { ysun = with inputs.self; [ hmModules.ysun.minimal ]; };
-    modules = with inputs; [
-      self.nixosModules.calibre
-      self.nixosModules.common
-      self.nixosModules.desktop
-      self.nixosModules.glance
-      self.nixosModules.minimal
-      self.nixosModules.passwordless
-      self.nixosModules.rebuild
-      self.nixosModules.server
-      self.nixosModules.uptime
-      srvos.nixosModules.common
-      srvos.nixosModules.server
-    ];
-    nixpkgs = inputs.rpi.inputs.nixpkgs.legacyPackages.${platform};
-    specialArgs = { inherit inputs lib; };
-  });
+  colmena =
+    let
+      os = "nixos";
+      specialArgs = { inherit inputs lib; };
+
+      users = { ysun = with inputs.self; [ hmModules.ysun.minimal ]; };
+
+      modules = with inputs; [
+        self.nixosModules.acme
+        self.nixosModules.as10779
+        self.nixosModules.attic
+        self.nixosModules.caddy
+        self.nixosModules.calibre
+        self.nixosModules.common
+        self.nixosModules.desktop
+        self.nixosModules.glance
+        self.nixosModules.golink
+        self.nixosModules.grafana
+        self.nixosModules.maxmind
+        self.nixosModules.minimal
+        self.nixosModules.passwordless
+        self.nixosModules.plausible
+        self.nixosModules.prometheus
+        self.nixosModules.rebuild
+        self.nixosModules.server
+        self.nixosModules.uptime
+        srvos.nixosModules.common
+        srvos.nixosModules.server
+      ];
+    in
+    (mkColmena rec {
+      inherit os inputs specialArgs modules users stateVersion;
+      platform = "x86_64-linux";
+      nixpkgs = (getSystem platform).allModuleArgs.pkgs;
+      hosts = [
+        "butte" # Virtua Cloud, 1 vCPU, 1GB RAM, 20GB Storage
+        "halti" # Garnix.io Hosting, test server
+        "highline" # Neptune Networks, 1 vCPU, 1GB RAM, 10GB Storage
+        "kongo" # Vultr, 1 vCPU, 1GB RAM, 25GB Storage
+        "lagern" # AWS, T3.Large, 25GB Storage
+        "odake" # SSDNodes NRT Performance, 8 vCPU, 32GB RAM, 640GB Storage
+        "toompea" # V.PS Pro Tallinn, 4 vCPU, 4GB RAM, 40GB Storage
+        "walberla" # Hetzner Cloud CX32, 4 vCPU, 8GB RAM, 80GB Storage
+      ];
+    }) // (mkColmena rec {
+      inherit os inputs specialArgs modules users stateVersion;
+      platform = "aarch64-linux";
+      nixpkgs = inputs.rpi.inputs.nixpkgs.legacyPackages.${platform};
+      hosts = [
+        "isere" # Raspberry Pi 4, 8GB RAM, 500GB Storage
+      ];
+    });
 
   colmenaHive = lib.makeHive colmena;
 
