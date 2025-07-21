69743a4321564390242a2aac5537aa4bba0c8d90
Yifei Sun <ysun@hey.com>
2025-07-21 12:53:25 +0200

    modules: zellij init

--- modules/home/ysun/minimal.nix
+++ modules/home/ysun/minimal.nix
@@ -4,6 +4,7 @@
   imports = with inputs.self.homeManagerModules.ysun; [
     nushell
     tmux
+    # zellij
     zsh
   ];
 }
---
+++ modules/home/ysun/zellij/default.nix
@@ -0,0 +1,12 @@
+{
+  programs.zellij = {
+    enable = true;
+
+    settings = {
+      theme = "nord";
+
+      show_startup_tips = false;
+      show_release_notes = false;
+    };
+  };
+}
