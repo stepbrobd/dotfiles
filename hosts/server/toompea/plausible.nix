89d66b8a8740748e5681db496001269d81c11889
Yifei Sun <ysun@hey.com>
2025-08-08 16:37:10 +0200

    modules: drop rkt.lol

--- hosts/server/toompea/plausible.nix
+++ hosts/server/toompea/plausible.nix
@@ -2,6 +2,6 @@
   services.plausible = {
     enable = true;
     mainDomain = "stats.ysun.co";
-    extraDomains = [ "stats.grenug.fr" "stats.rkt.lol" ];
+    extraDomains = [ "stats.grenug.fr" ];
   };
 }
