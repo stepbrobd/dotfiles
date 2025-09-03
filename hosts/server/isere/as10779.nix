feceda23c4568bbd1178cb36cc63740bf7457e2c
Yifei Sun <ysun@hey.com>
2025-09-03 18:08:00 +0200

    hosts: add ip addr

--- hosts/server/isere/as10779.nix
+++ hosts/server/isere/as10779.nix
@@ -1,8 +1,31 @@
+{ config, ... }:
+
 {
-  services.tailscale.extraSetFlags = [
-    "--accept-routes"
-    "--advertise-exit-node"
-    "--advertise-routes=23.161.104.131/32,2602:f590:0::23:161:104:131/128"
-    "--snat-subnet-routes=false"
-  ];
+  services.as10779 = {
+    enable = true;
+    router.exit = false;
+
+    local = {
+      hostname = config.networking.hostName;
+      interface = {
+        local = "dummy0";
+        primary = "end0";
+      };
+      ipv4.addresses = [
+        "23.161.104.131/32"
+      ];
+      ipv6.addresses = [
+        "2602:f590:0::23:161:104:131/128"
+      ];
+    };
+
+    router.static = {
+      ipv4.routes = [
+        { prefix = "23.161.104.0/24"; option = "reject"; }
+      ];
+      ipv6.routes = [
+        { prefix = "2602:f590::/36"; option = "reject"; }
+      ];
+    };
+  };
 }
