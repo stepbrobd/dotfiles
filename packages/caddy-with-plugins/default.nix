910c69fd82fe326f79a136e1e6d1a9b3bc81ae40
Yifei Sun <ysun@hey.com>
2025-09-11 09:02:58 +0200

    packages: bump caddy plugins

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -5,9 +5,9 @@ caddy.withPlugins {
     "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
     "github.com/caddy-dns/cloudflare@v0.2.2-0.20250724223520-f589a18c0f5d"
     "github.com/caddyserver/cache-handler@v0.16.0"
-    "github.com/darkweak/storages/badger/caddy@v0.0.15"
+    "github.com/darkweak/storages/badger/caddy@v0.0.16"
     "github.com/ueffel/caddy-brotli@v1.6.0"
-    "github.com/darkweak/souin=github.com/darkweak/souin@v1.7.8-0.20250812092955-b2a08bf1141e"
+    "github.com/darkweak/souin=github.com/darkweak/souin@v1.7.8-0.20250907151505-548a57d94fc5"
   ];
-  hash = "sha256-attsfFfGgikZoqof3fIJA3aLn2ZoXBgCR2zv2EZVTbA=";
+  hash = "sha256-xbq+PvgEHYwDC64S0jXVr50zObThfWOFjWLTrBf9p4o=";
 }
