da2eec3bd407f747a16da17cf321e9781e8e1f30
Yifei Sun <ysun@hey.com>
2025-09-18 17:38:01 +0200

    packages: some more nixvim tweaks

--- packages/nixvim/completion.nix
+++ packages/nixvim/completion.nix
@@ -1,117 +1,85 @@
 # https://github.com/MikaelFangel/nixvim-config/blob/92924c1938e48fd3b77166e616ae6e6bd4a5587b/config/cmp.nix
 
 {
-  plugins = {
-    coq-nvim.enable = true;
+  plugins.cmp = {
+    enable = true;
 
-    cmp = {
-      enable = true;
+    settings = {
+      completion.completeopt = "menu,menuone,noinsert";
+      experimental.ghost_text = true;
 
-      settings = {
-        completion.completeopt = "menu,menuone,noinsert";
-        experimental.ghost_text = true;
+      snippet.expand = ''
+        function(args)
+          require('luasnip').lsp_expand(args.body)
+        end
+      '';
 
-        snippet.expand = ''
-          function(args)
-            require('luasnip').lsp_expand(args.body)
-          end
-        '';
+      autoEnableSources = true;
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "nvim_lsp_signature_help"; }
+        { name = "luasnip"; }
+        { name = "nvim_lsp_document_symbol"; }
+        { name = "treesitter"; }
+        { name = "nvim_lua"; }
+        { name = "calc"; }
+        { name = "dap"; }
+        { name = "path"; }
+        { name = "git"; }
+        { name = "buffer"; }
+        { name = "rg"; }
+        { name = "latex_symbols"; }
+        { name = "dictionary"; }
+        { name = "spell"; }
+        { name = "emoji"; }
+      ];
 
-        autoEnableSources = true;
-        sources = [
-          {
-            name = "buffer";
-            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
-          }
-          { name = "calc"; }
-          { name = "cmdline"; }
-          { name = "coq"; }
-          { name = "dap"; }
-          { name = "dictionary"; }
-          { name = "emoji"; }
-          { name = "git"; }
-          { name = "greek"; }
-          { name = "latex_symbols"; }
-          { name = "look"; }
-          { name = "luasnip"; }
-          { name = "nvim_lsp"; }
-          { name = "nvim_lsp_document_symbol"; }
-          { name = "nvim_lsp_signature_help"; }
-          { name = "nvim_lua"; }
-          { name = "path"; }
-          { name = "rg"; }
-          { name = "spell"; }
-          { name = "treesitter"; }
-          { name = "vsnip"; }
-          { name = "yanky"; }
-          { name = "zsh"; }
-        ];
+      performance = {
+        debounce = 50;
+        fetching_timeout = 250;
+        max_view_entries = 50;
+      };
 
-        # window = {
-        #   completion = {
-        #     winhighlight =
-        #       "FloatBorder:CmpBorder,Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel";
-        #     scrollbar = false;
-        #     sidePadding = 0;
-        #     border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
-        #   };
-        #   settings.documentation = {
-        #     border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
-        #     winhighlight =
-        #       "FloatBorder:CmpBorder,Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel";
-        #   };
-        # };
+      formatting = {
+        expandable_indicator = true;
+        fields = [ "kind" "abbr" "menu" ];
+      };
 
-        mapping = {
-          "<C-n>" = "cmp.mapping.select_next_item()";
-          "<C-p>" = "cmp.mapping.select_prev_item()";
-          "<C-j>" = "cmp.mapping.select_next_item()";
-          "<C-k>" = "cmp.mapping.select_prev_item()";
-          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<S-Tab>" = "cmp.mapping.close()";
-          "<Tab>" =
-            # lua 
-            ''
-              function(fallback)
-                local line = vim.api.nvim_get_current_line()
-                if line:match("^%s*$") then
-                  fallback()
-                elseif cmp.visible() then
-                  cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
-                else
-                  fallback()
-                end
-              end
-            '';
-          "<Down>" =
-            # lua
-            ''
-              function(fallback)
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif require("luasnip").expand_or_jumpable() then
-                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-                else
-                  fallback()
-                end
-              end
-            '';
-          "<Up>" =
-            # lua
-            ''
-              function(fallback)
-                if cmp.visible() then
-                  cmp.select_prev_item()
-                elseif require("luasnip").jumpable(-1) then
-                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-                else
-                  fallback()
-                end
-              end
-            '';
-        };
+      window = {
+        completion.__raw = ''cmp.config.window.bordered()'';
+        documentation.__raw = ''cmp.config.window.bordered()'';
+      };
+
+      # window = {
+      #   completion = {
+      #     winhighlight =
+      #       "FloatBorder:CmpBorder,Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel";
+      #     scrollbar = false;
+      #     sidePadding = 0;
+      #     border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
+      #   };
+      #   settings.documentation = {
+      #     border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
+      #     winhighlight =
+      #       "FloatBorder:CmpBorder,Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel";
+      #   };
+      # };
+
+      mapping = {
+        "<Down>" = "cmp.mapping.select_next_item()";
+        "<Tab>" = "cmp.mapping.select_next_item()";
+        "<C-n>" = "cmp.mapping.select_next_item()";
+        "<C-j>" = "cmp.mapping.select_next_item()";
+
+        "<Up>" = "cmp.mapping.select_prev_item()";
+        "<C-p>" = "cmp.mapping.select_prev_item()";
+        "<C-k>" = "cmp.mapping.select_prev_item()";
+
+        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<S-Tab>" = "cmp.mapping.close()";
       };
     };
   };
--- packages/nixvim/lsp.nix
+++ packages/nixvim/lsp.nix
@@ -1,52 +1,41 @@
-{ pkgs, ... }:
-
 {
   plugins.lsp.enable = true;
+  plugins.lsp.inlayHints = true;
   plugins.lspkind.enable = true;
+
   plugins.lsp-format = {
     enable = true;
     lspServersToEnable = "all";
   };
-
-  plugins.none-ls = {
-    enable = true;
-    enableLspFormat = true;
-  };
+  # plugins.none-ls = {
+  #   enable = true;
+  #   enableLspFormat = true;
+  # };
 
   # C/C++
-  plugins.lsp.servers.cmake.enable = true;
   plugins.lsp.servers.clangd.enable = true;
 
   # Coq
-  plugins.lsp.servers.coq_lsp.enable = false;
-  extraPlugins = with pkgs.vimPlugins; [ Coqtail ];
+  plugins.lsp.servers.coq_lsp = {
+    enable = true;
+    package = null; # pkgs.coqPackages.coq-lsp;
+  };
 
   # Go
   plugins.lsp.servers.gopls.enable = true;
 
   # HTML/JS/TS/CSS
-  plugins.lsp.servers.cssls.enable = true; # CSS
+  plugins.lsp.servers.cssls.enable = true;
   plugins.lsp.servers.denols = {
     enable = true;
-    extraOptions = {
-      single_file_support = true;
-      init_options = {
-        lint = true;
-        unstable = true;
-        suggest.imports.hosts."https://deno.land" = true;
-      };
-      root_dir = "require('lspconfig').util.root_pattern('deno.json', 'deno.jsonc')";
-    };
+    extraOptions.root_dir = "require('lspconfig').util.root_pattern('deno.json', 'deno.jsonc')";
   };
-  plugins.lsp.servers.html.enable = true; # HTML
-  plugins.lsp.servers.tailwindcss.enable = true; # TailwindCSS
-  plugins.lsp.servers.ts_ls.enable = true; # TS/JS
-
-  # JSON
-  plugins.lsp.servers.jsonls.enable = true;
-
-  # Lean
-  plugins.lean.enable = false;
+  plugins.lsp.servers.html.enable = true;
+  plugins.lsp.servers.tailwindcss.enable = true;
+  plugins.lsp.servers.ts_ls = {
+    enable = true;
+    extraOptions.root_dir = "require('lspconfig').util.root_pattern('package.json', 'tsconfig.json', 'jsconfig.json')";
+  };
 
   # Markdown
   plugins.lsp.servers.marksman.enable = true;
@@ -56,7 +45,6 @@
   plugins.lsp.servers.nixd = {
     enable = true;
     extraOptions.offset_encoding = "utf-8"; # nixvim#2390
-    settings.expr = "import <nixpkgs> { }";
   };
   plugins.nix.enable = true;
   plugins.nix-develop.enable = true;
@@ -65,9 +53,7 @@
   plugins.lsp.servers.ocamllsp.enable = true;
 
   # Python
-  plugins.lsp.servers.jedi_language_server.enable = true;
   plugins.lsp.servers.pyright.enable = true;
-  plugins.lsp.servers.pylsp.enable = true;
   plugins.lsp.servers.ruff.enable = true;
 
   # Shell
@@ -88,10 +74,6 @@
 
   # Typst
   plugins.lsp.servers.tinymist.enable = true;
-  plugins.typst-vim = {
-    enable = true;
-    settings.pdf_viewer = "zathura";
-  };
 
   # YAML
   plugins.lsp.servers.yamlls.enable = true;
