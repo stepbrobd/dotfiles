9817d4fc7acd2893b4a4c938c0042b3606d81a98
Yifei Sun <ysun@hey.com>
2025-09-18 18:41:08 +0200

    users: change the source of ssh keys

--- users/ysun/default.nix
+++ users/ysun/default.nix
@@ -15,12 +15,7 @@
       else
         abort "Unsupported OS";
 
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBVZ9mzYNxccuh3uQR7Hly4KjhbRh4s6UlGQe2GjMtIC" # framework
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHaMDj2MpMGwDcUfDcfNHb9UR7gA5Pgtt4EPyC+1OkBP" # xps
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw" # macbook
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQCjwNmB60FQhDncIyX/wCRAPIlLD5KLAGrgAdt4xGw" # servercat
-    ];
+    openssh.authorizedKeys = { inherit (lib.blueprint.users.ysun.openssh.authorizedKeys) keys; };
   } // lib.optionalAttrs pkgs.stdenv.isLinux {
     isNormalUser = true;
     extraGroups = [
