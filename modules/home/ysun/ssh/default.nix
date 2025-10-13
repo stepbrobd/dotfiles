7cf1cb0f83ff7af88410341486ab1bf0548ea1b7
Yifei Sun <ysun@hey.com>
2025-10-13 09:06:43 +0200

    modules: get rid of old utokyo ssh config

--- modules/home/ysun/ssh/default.nix
+++ modules/home/ysun/ssh/default.nix
@@ -13,56 +13,15 @@
       hashKnownHosts = lib.mkDefault false;
     };
 
-    extraConfig =
-      let
-        proxy = "ProxyCommand ssh -W %h:%p yifei@133.11.234.34";
-        targets = {
-          yifei = [
-            { ceylon = "133.11.234.39"; }
-            { uva = "133.11.234.42"; }
-            { vpi = "172.19.0.3"; }
-          ];
-          plask = [
-            { plask = "133.11.234.43"; }
-          ];
-        };
-        gen = targets:
-          let
-            users = lib.attrNames targets;
-            configs = lib.concatLists (lib.map
-              (user:
-                let
-                  hosts = targets.${user};
-                  userConfigs = lib.map
-                    (hostAttr:
-                      let
-                        host = lib.elemAt (lib.attrNames hostAttr) 0;
-                        ip = hostAttr.${host};
-                      in
-                      ''
-                        Host ${host}
-                          User ${user}
-                          ${proxy}
-                          HostName ${ip}
-                      ''
-                    )
-                    hosts;
-                in
-                userConfigs
-              )
-              users);
-          in
-          lib.concatStringsSep "\n" configs;
-      in
-      gen targets + "\n" + ''
-        Host g5k
-          User yisun
-          Hostname access.grid5000.fr
+    extraConfig = "\n" + ''
+      Host g5k
+        User yisun
+        Hostname access.grid5000.fr
 
-        Host *.g5k
-          User yisun
-          ProxyCommand sh -c 'ssh g5k -W "$(basename %h .g5k):%p"'
-      ''
+      Host *.g5k
+        User yisun
+        ProxyCommand sh -c 'ssh g5k -W "$(basename %h .g5k):%p"'
+    ''
       /* + "\n" +
       (if pkgs.stdenv.isLinux then ''
       Host *
