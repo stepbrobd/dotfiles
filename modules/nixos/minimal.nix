56ad76f12a3179c03fdcae8f0488c8e0847b283d
Yifei Sun <ysun@hey.com>
2025-10-23 10:59:44 +0200

    modules: fix ordering

--- modules/nixos/minimal.nix
+++ modules/nixos/minimal.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 
 {
   imports = with inputs.self.nixosModules; [ fail2ban nftables mglru tailscale time vxlan ];
@@ -8,7 +8,9 @@
   # services.prometheus.enable = true;
 
   systemd.services.network-local-commands = {
-    wants = [ "network-online.target" ];
-    after = [ "network-online.target" ];
+    before = lib.mkForce [ ];
+    after = lib.mkForce [ "network-online.target" "systemd-networkd.service" ];
+    wants = lib.mkForce [ "network-online.target" ];
+    wantedBy = lib.mkForce [ "multi-user.target" ];
   };
 }
