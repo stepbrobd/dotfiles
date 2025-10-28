d992efcebea47ac49848255c05ce0f251e117ec7
Yifei Sun <ysun@hey.com>
2025-10-28 21:53:11 +0100

    sbcl: drop (useless since upstream code is hard to compose with my own overlay)
    https://github.com/hraban/mac-app-util/blob/8414fa1e2cb775b17793104a9095aabeeada63ef/flake.nix#L97
    
    lets just wait until the sbcl fix is propagated to unstable then

--- pkgs/sbcl/default.nix
+++
@@ -1,9 +0,0 @@
-{ stdenv
-, pkgs
-, pkgsPrev ? pkgs # `pkgsPrev` only provided in overlays
-}:
-
-if stdenv.isDarwin
-# nixpkgs#456347 nixpkgs#455569 nixpkgs#456541
-then pkgsPrev.sbcl.overrideAttrs { doCheck = false; }
-else pkgsPrev.sbcl
