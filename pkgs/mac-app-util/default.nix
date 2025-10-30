cf57ccc36253c0628f57995b22e73db310c7a904
Yifei Sun <ysun@hey.com>
2025-10-30 20:33:56 +0100

    pkgs/mac-app-util: use sbcl 2.5.5

--- pkgs/mac-app-util/default.nix
+++ pkgs/mac-app-util/default.nix
@@ -1,17 +1,18 @@
-{ lib
-, inputs
+{ inputs
 , stdenv
+, pkgs
 }:
 
-let
-  noCheckForSbcl257Plz = p:
-    if lib.isDerivation p && p.name == "sbcl" && p.version == "2.5.7" then
-      p.overrideAttrs
-        (prev: {
-          doCheck = false;
-          nativeBuildInputs = if prev ? nativeBuildInputs then lib.map noCheckForSbcl257Plz prev.nativeBuildInputs else [ ];
-          buildInputs = if prev ? buildInputs then lib.map noCheckForSbcl257Plz prev.buildInputs else [ ];
-        })
-    else p;
-in
-noCheckForSbcl257Plz inputs.trampoline.packages.${stdenv.hostPlatform.system}.default
+inputs.trampoline.packages.${stdenv.hostPlatform.system}.default.overrideAttrs {
+  doCheck = false;
+  buildInputs = [
+    (pkgs.sbcl_2_5_5.withPackages (_: with _; [
+      alexandria
+      cl-interpol
+      cl-json
+      inferior-shell
+      str
+      trivia
+    ]))
+  ];
+}
