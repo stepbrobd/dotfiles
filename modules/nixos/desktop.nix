96cb8d647170d9b6aa3d54f8f581304255dff118
Yifei Sun <ysun@hey.com>
2025-11-08 13:41:56 +0100

    modules/nixos/desktop: cleanup plymouth a bit

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -26,26 +26,12 @@ in
     # disable boot logs when using a desktop manager
     {
       boot = {
-        consoleLogLevel = 0;
-        initrd = {
-          verbose = false;
-          systemd.enable = true;
+        kernelParams = [ "quiet" ];
+        initrd.systemd.enable = true;
+        plymouth = {
+          enable = true;
+          font = "${pkgs.noto-fonts}/share/fonts/noto/NotoSans[wdth,wght].ttf";
         };
-        kernelParams = [
-          "quiet"
-          "loglevel=3"
-          "udev.log_level=3"
-          "boot.shell_on_fail"
-          "rd.udev.log_level=3"
-          "systemd.show_status=auto"
-          "vt.global_cursor_default=0"
-        ];
-      };
-
-      # boot.loader.grub.theme = pkgs.nixos-grub2-theme;
-      boot.plymouth = {
-        enable = true;
-        font = "${pkgs.nerd-fonts.intone-mono}/share/fonts/truetype/NerdFonts/IntoneMono/IntoneMonoNerdFont-Regular.ttf";
       };
     }
 
