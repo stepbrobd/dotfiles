2a43afb6ba963d7a6a1dc293110a8efd053fbd26
Yifei Sun <ysun@hey.com>
2025-11-14 14:28:52 +0100

    modules/nixos/vaultwarden: init

---
+++ modules/nixos/vaultwarden.nix
@@ -0,0 +1,89 @@
+{ lib, ... }:
+
+{ config, ... }:
+
+let
+  cfg = config.services.vaultwarden;
+  domain = "vault.ysun.co";
+in
+{
+  config = lib.mkIf cfg.enable {
+    sops.secrets.vaultwarden = { };
+    services.vaultwarden = {
+      backupDir = "/var/backup/vaultwarden/";
+
+      environmentFile = config.sops.secrets.vaultwarden.path;
+
+      config = {
+        DOMAIN = "https://${domain}";
+        ROCKET_LOG = "critical";
+        ROCKET_ADDRESS = "::";
+        ROCKET_PORT = 9999;
+        SIGNUPS_ALLOWED = false;
+        SHOW_PASSWORD_HINT = false;
+        ORG_EVENTS_ENABLED = true;
+      };
+    };
+
+    services.caddy = {
+      enable = true;
+
+      # put headers here just in case
+      # most likely i'll only be using this behind tailscale
+      virtualHosts.${domain}.extraConfig = ''
+        import common
+        reverse_proxy ${lib.toString cfg.config.ROCKET_ADDRESS}:${lib.toString cfg.config.ROCKET_PORT} {
+          vars realip {remote_host}
+          @cf header CF-Connecting-IP *
+          vars @cf realip {header.CF-Connecting-IP}
+          @xf header X-Forwarded-For *
+          vars @xf realip {header.X-Forwarded-For}
+          header_up X-Real-IP {vars.realip}
+        }
+      '';
+    };
+
+    services.fail2ban = {
+      enable = true;
+
+      jails = {
+        vaultwarden-web = {
+          filter = {
+            INCLUDES.before = "common.conf";
+            Definition = {
+              failregex = "^.*Username or password is incorrect\. Try again\. IP: <ADDR>\. Username:.*$";
+              ignoreregex = "";
+            };
+          };
+          settings = {
+            backend = "systemd";
+            port = "80,443";
+            filter = "vaultwarden-web[journalmatch='_SYSTEMD_UNIT=vaultwarden.service']";
+            banaction = "%(banaction_allports)s";
+            maxretry = 3;
+            bantime = 14400;
+            findtime = 14400;
+          };
+        };
+        vaultwarden-admin = {
+          filter = {
+            INCLUDES.before = "common.conf";
+            Definition = {
+              failregex = "^.*Invalid admin token\. IP: <ADDR>.*$";
+              ignoreregex = "";
+            };
+          };
+          settings = {
+            backend = "systemd";
+            port = "80,443";
+            filter = "vaultwarden-admin[journalmatch='_SYSTEMD_UNIT=vaultwarden.service']";
+            banaction = "%(banaction_allports)s";
+            maxretry = 3;
+            bantime = 14400;
+            findtime = 14400;
+          };
+        };
+      };
+    };
+  };
+}
