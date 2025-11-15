f78adaca424461d996beadf92e705823431930d2
Yifei Sun <ysun@hey.com>
2025-11-15 16:11:42 +0100

    modules/{darwin,nixos}/fonts: remove pixelmplus

--- modules/darwin/fonts.nix
+++ modules/darwin/fonts.nix
@@ -6,7 +6,6 @@
     font-awesome
     nerd-fonts.intone-mono
     nerd-fonts.jetbrains-mono
-    pixelmplus
     san-francisco
   ];
 }
--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -14,7 +14,6 @@
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-color-emoji
-      pixelmplus
     ];
 
     fontconfig = {
