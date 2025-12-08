f7b30f333b15ef90d2d8a76414e1019a98b3c624
Yifei Sun <ysun@hey.com>
2025-12-08 16:44:26 +0100

    pkgs/caddy: bump deps

--- pkgs/caddy-with-plugins/default.nix
+++ pkgs/caddy-with-plugins/default.nix
@@ -1,14 +1,15 @@
 { caddy }:
 
 caddy.withPlugins {
-  hash = "sha256-wGggjMnsjMmIlgBaCm4NguRawRJDaLNkEYq2WpSVXvk=";
+  hash = "sha256-/IwJUSWRgKJ+SH2oUGa3GqPaRPBvliTuTmFf2u8vloY=";
   plugins = [
     "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
-    "github.com/caddy-dns/cloudflare@v0.2.2-0.20250724223520-f589a18c0f5d"
+    "github.com/caddy-dns/cloudflare@v0.2.3-0.20251204174556-6dc1fbb7e925"
     "github.com/caddyserver/cache-handler@v0.16.0"
-    "github.com/darkweak/souin=github.com/darkweak/souin@v1.7.8-0.20250907151505-548a57d94fc5"
-    "github.com/darkweak/storages/badger/caddy@v0.0.15" # getting storage backend error on 0.0.16
-    "github.com/tailscale/caddy-tailscale@v0.0.0-20250915161136-32b202f0a953"
+    "github.com/darkweak/souin@v1.7.8"
+    "github.com/darkweak/storages/badger/caddy@v0.0.16"
+    "github.com/ss098/certmagic-s3@v0.0.0-20250922022452-8af482af5f39"
+    "github.com/tailscale/caddy-tailscale@v0.0.0-20251204171825-f070d146dd61"
     "github.com/ueffel/caddy-brotli@v1.6.0"
   ];
 }
