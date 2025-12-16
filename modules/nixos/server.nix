7dd050454b382a37eece0597729691737c300048
Yifei Sun <ysun@hey.com>
2025-12-16 13:07:48 +0100

    modules/nixos/server: still need macs

--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -37,7 +37,10 @@
         "sntrup761x25519-sha512@openssh.com"
       ];
       settings.Ciphers = lib.mkForce [ "chacha20-poly1305@openssh.com" ];
-      settings.Macs = lib.mkForce null;
+      settings.Macs = lib.mkForce [
+        "hmac-sha2-256-etm@openssh.com"
+        "hmac-sha2-512-etm@openssh.com"
+      ];
       banner = ''
          ▄▄  ▗          ▗▄▄         ▗▄▄ ▗▄▖         ▗▄▄             
         ▐▘ ▘▗▟▄  ▄▖ ▗▄▖ ▐  ▌ ▖▄  ▄▖ ▐  ▌▐ ▝▖         ▐  ▗▗▖  ▄▖     
