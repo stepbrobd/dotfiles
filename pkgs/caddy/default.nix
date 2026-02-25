add7015fb6a87d499f6a0f27a8f8342ffdc1af60
Yifei Sun <ysun@hey.com>
2026-02-25 17:28:27 +0100

    pkgs/caddy: bump deps

--- pkgs/caddy/default.nix
+++ pkgs/caddy/default.nix
@@ -3,13 +3,13 @@
 # pull caddy from `inputs` directly to prevent infinite recursion
 # as `caddy.withPlugins` is implemented with override
 inputs.nixpkgs.legacyPackages.${stdenv.hostPlatform.system}.caddy.withPlugins {
-  hash = "sha256-0CXDC8Pw/VTDNRCk2SGzdu/mE7QEbbZ73kCrSSDsoCE=";
+  hash = "sha256-TwpeDbbZZoIraKkT4kYZEOnym2s2qeZzeNhacTZON+w=";
   plugins = [
     "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
     "github.com/caddy-dns/cloudflare@v0.2.3-0.20251204174556-6dc1fbb7e925"
     "github.com/caddyserver/cache-handler@v0.16.0"
     "github.com/darkweak/souin@v1.7.8"
-    "github.com/darkweak/storages/badger/caddy@v0.0.17"
+    "github.com/darkweak/storages/badger/caddy@v0.0.18"
     "github.com/ss098/certmagic-s3@v0.0.0-20250922022452-8af482af5f39"
     "github.com/tailscale/caddy-tailscale@v0.0.0-20260106222316-bb080c4414ac"
     "github.com/ueffel/caddy-brotli@v1.6.0"
