5733a3cad07f884d1adee3cb1e01f5e12b2560f5
Yifei Sun <ysun@hey.com>
2026-02-26 22:16:46 +0100

    modules/flake/packages: eagerly filter packages

--- modules/flake/packages.nix
+++ modules/flake/packages.nix
@@ -3,12 +3,16 @@
 {
   perSystem =
     { pkgs, ... }:
-    rec {
+    {
       legacyPackages = lib.localPackagesFrom {
         dir = ../../pkgs;
         scope = pkgs;
       };
 
-      packages = legacyPackages;
+      packages =
+        lib.genAttrs
+          # note that this is eager but i cant think of a better way to delay eval
+          (lib.filter (p: lib.isDerivation pkgs.${p}) (lib.childDirsWithDefault ../../pkgs))
+          (p: pkgs.${p});
     };
 }
