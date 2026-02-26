8a5b52539a55da85604787f4c58fe18a9e6660b9
Yifei Sun <ysun@hey.com>
2026-02-26 22:27:10 +0100

    modules/flake/packages: revert eagerly filter packages
    This reverts commit 5733a3cad07f884d1adee3cb1e01f5e12b2560f5.

--- modules/flake/packages.nix
+++ modules/flake/packages.nix
@@ -3,16 +3,12 @@
 {
   perSystem =
     { pkgs, ... }:
-    {
+    rec {
       legacyPackages = lib.localPackagesFrom {
         dir = ../../pkgs;
         scope = pkgs;
       };
 
-      packages =
-        lib.genAttrs
-          # note that this is eager but i cant think of a better way to delay eval
-          (lib.filter (p: lib.isDerivation pkgs.${p}) (lib.childDirsWithDefault ../../pkgs))
-          (p: pkgs.${p});
+      packages = legacyPackages;
     };
 }
