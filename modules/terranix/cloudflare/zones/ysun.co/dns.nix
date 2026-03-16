e65dfd726f3cc0bcfd0085cb33da9a9345e2055e
Yifei Sun <ysun@hey.com>
2026-03-16 18:15:20 +0100

    modules/terranix/cloudflare: dont proxy grafana?

--- modules/terranix/cloudflare/zones/ysun.co/dns.nix
+++ modules/terranix/cloudflare/zones/ysun.co/dns.nix
@@ -257,7 +257,7 @@ in
 
       co_ysun_otel = {
         type = "CNAME";
-        proxied = true;
+        proxied = false;
         name = "otel";
         content = "halti.sd.ysun.co";
         comment = "Garnix - Grafana";
