3c71e75601593f00a43ab5debb9869df43584550
Yifei Sun <ysun@hey.com>
2026-03-17 17:15:09 +0100

    pkgs/miroir: dont wrap

--- pkgs/miroir/default.nix
+++ pkgs/miroir/default.nix
@@ -1,15 +1,3 @@
-{ inputs
-, stdenv
-, makeWrapper
-}:
+{ inputs, stdenv }:
 
-let
-  config = ../../repos/config.toml;
-in
-inputs.miroir.packages.${stdenv.hostPlatform.system}.default.overrideAttrs (prev: {
-  nativeBuildInputs = prev.nativeBuildInputs or [ ] ++ [ makeWrapper ];
-  postFixup = prev.postFixup or "" + ''
-    wrapProgram $out/bin/${prev.meta.mainProgram} \
-      --set MIROIR_CONFIG ${config}
-  '';
-})
+inputs.miroir.packages.${stdenv.hostPlatform.system}.default
