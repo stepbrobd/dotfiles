274a0d3b3c9f24b473609aad2e6c2526d9a08661
Yifei Sun <ysun@hey.com>
2026-03-21 10:30:55 +0100

    modules/nixos/home-assistant: test m reso gtfs feed

--- modules/nixos/home-assistant.nix
+++ modules/nixos/home-assistant.nix
@@ -47,14 +47,21 @@ in
         auth_oidc
         midea_ac_lan
         spook
+        # remove after https://nixpkgs-tracker.ocfox.me/?pr=500818 hits unstable
         (gtfs-realtime.overrideAttrs {
           version = "0.4.4";
-          src = fetchFromGitHub {
+          src = pkgs.fetchFromGitHub {
             owner = "bcpearce";
             repo = "homeassistant-gtfs-realtime";
             tag = "0.4.4";
             hash = "sha256-iT32ADgDHwBnX6v4UnGkUNtGL9/ukA3EMCLquCBcaKo=";
           };
+          # gtfs-rt feeds for grenoble were previously served at data.metromobilite.fr but that domain has been decommed
+          # the replacement api at data.mobilites-m.fr serves static gtfs data but does not expose gtfs-rt
+          # realtime data is only available via proprietary json api:
+          # - https://data.mobilites-m.fr/api/routers/default/index/clusters/{SEM:id}/stoptimes
+          # - see: https://www.mobilites-m.fr/pages/opendata/OpenDataApi.html
+          # - static feed: https://data.mobilites-m.fr/api/gtfs/SEM
         })
       ];
     };
