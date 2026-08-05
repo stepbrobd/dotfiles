8351ea296eab473eead3623f8cdc247ece072664
Yifei Sun <ysun@hey.com>
2026-08-05 21:49:19 +0200

    pkgs/kanidm: bump to 1.11

--- pkgs/kanidm/default.nix
+++ pkgs/kanidm/default.nix
@@ -1,6 +1,6 @@
-{ kanidmWithSecretProvisioning_1_10 }:
+{ kanidmWithSecretProvisioning_1_11 }:
 
-kanidmWithSecretProvisioning_1_10.overrideAttrs (prev: {
+kanidmWithSecretProvisioning_1_11.overrideAttrs (prev: {
   # the patch probably only need to inject plausible script now
   # but lets just leave private cache and csp as is in there
   patches = prev.patches ++ [ ./custom-deployment.patch ];
