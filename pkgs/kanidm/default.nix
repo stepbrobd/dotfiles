77f6db10a3cc98a4e4f7bf5882fa53abe512eeab
Yifei Sun <ysun@hey.com>
2026-08-05 22:27:54 +0200

    pkgs/kanidm: fix patch and add nord theme override css

--- pkgs/kanidm/default.nix
+++ pkgs/kanidm/default.nix
@@ -1,10 +1,11 @@
 { kanidmWithSecretProvisioning_1_11 }:
 
 kanidmWithSecretProvisioning_1_11.overrideAttrs (prev: {
-  # the patch probably only need to inject plausible script now
-  # but lets just leave private cache and csp as is in there
   patches = prev.patches ++ [ ./custom-deployment.patch ];
 
-  # some darwin test failures?
+  postPatch = (prev.postPatch or "") + ''
+    cp ${./override.css} server/core/static/override.css
+  '';
+
   __darwinAllowLocalNetworking = true;
 })
