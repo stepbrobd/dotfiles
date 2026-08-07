e6f67460290059634079023788336b96f4a1d15c
Yifei Sun <ysun@hey.com>
2026-08-07 21:57:00 +0200

    flake: drop nix-darwin override

--- flake.nix
+++ flake.nix
@@ -76,9 +76,7 @@
     compat.flake = false;
     cornflake.url = "github:jzbor/cornflakes";
     crane.url = "github:ipetkov/crane";
-    # TODO: drop after https://github.com/nix-darwin/nix-darwin/issues/1817
-    darwin.url = "github:stepbrobd/nix-darwin/man";
-    # darwin.url = "github:nix-darwin/nix-darwin";
+    darwin.url = "github:nix-darwin/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
