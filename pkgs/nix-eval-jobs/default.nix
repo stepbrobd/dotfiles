c43e807853947dad5981546dbac376726d43149f
Yifei Sun <ysun@hey.com>
2026-08-08 20:19:59 +0200

    pkgs/nix-eval-jobs: use 2.34.3

--- pkgs/nix-eval-jobs/default.nix
+++ pkgs/nix-eval-jobs/default.nix
@@ -1,15 +1,13 @@
 { pkgsPrev
-, fetchpatch2
+, fetchFromGitHub
+, nixVersions
 }:
 
-pkgsPrev.nix-eval-jobs.overrideAttrs (prev: {
-  patches = (prev.patches or [ ]) ++ [
-    # FIXME:
-    # https://github.com/NixOS/nix-eval-jobs/pull/429
-    # temporary patch for https://github.com/NixOS/nix-eval-jobs/issues/430
-    (fetchpatch2 {
-      url = "https://github.com/NixOS/nix-eval-jobs/commit/ff3b82d0895b68e2a802aa0ab6f726f269c785d8.patch";
-      hash = "sha256-U0X00SIBU+GLM4QY4lcaTX4rITQFc651oj2xH2aneOA=";
-    })
-  ];
-})
+let version = "2.34.3"; in (pkgsPrev.nix-eval-jobs.override { nixComponents = nixVersions.nixComponents_2_34; }).overrideAttrs {
+  src = fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nix-eval-jobs";
+    tag = "v${version}";
+    hash = "sha256-YaVQAgBxWbUBFHXLBLzdUyVvuA/DDw80SEnn9iq0Veo=";
+  };
+}
