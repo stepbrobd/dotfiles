54829d5b0d2b91ccc4e53b83de9e1441ed79e5fe
Yifei Sun <ysun@hey.com>
2026-08-10 13:27:20 +0200

    pkgs/nix-eval-jobs: 2.34.3 -> 2.35.1

--- pkgs/nix-eval-jobs/default.nix
+++ pkgs/nix-eval-jobs/default.nix
@@ -1,14 +1,13 @@
 { pkgsPrev
 , fetchFromGitHub
-, nixVersions
 }:
 
-let version = "2.34.3"; in (pkgsPrev.nix-eval-jobs.override { nixComponents = nixVersions.nixComponents_2_34; }).overrideAttrs {
-  inherit version;
+pkgsPrev.nix-eval-jobs.overrideAttrs (final: _: {
+  version = "2.35.1";
   src = fetchFromGitHub {
     owner = "NixOS";
     repo = "nix-eval-jobs";
-    tag = "v${version}";
-    hash = "sha256-YaVQAgBxWbUBFHXLBLzdUyVvuA/DDw80SEnn9iq0Veo=";
+    tag = "v${final.version}";
+    hash = "sha256-EFJnN35L7UieL8zV8qPrpqfdfzztWksY8JYuXF+mr9o=";
   };
-}
+})
