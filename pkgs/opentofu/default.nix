7eb412a08273a3a2d7788ae8ff5ab85b20999240
Yifei Sun <ysun@hey.com>
2026-08-15 19:20:16 +0200

    pkgs/opentofu: add arin provider

--- pkgs/opentofu/default.nix
+++ pkgs/opentofu/default.nix
@@ -1,9 +1,11 @@
-{ pkgsPrev, terraform-providers-bin }:
+{ pkgsPrev, terraform-providers-bin, terraform-provider-arin }:
 
-(pkgsPrev.opentofu.withPlugins (_: with terraform-providers-bin.providers; [
+pkgsPrev.opentofu.withPlugins (_: (with terraform-providers-bin.providers; [
   Backblaze.b2
   carlpett.sops
   cloudflare.cloudflare
   fastly.fastly
   tailscale.tailscale
-]))
+]) ++ [
+  terraform-provider-arin
+])
