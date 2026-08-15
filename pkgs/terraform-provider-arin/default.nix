9981b9c53d35da160cdf8c4c1da6e226455f25b4
Yifei Sun <ysun@hey.com>
2026-08-15 20:32:23 +0200

    pkgs/terraform-provider-arin: 2026.812.0 -> 2026.815.0

--- pkgs/terraform-provider-arin/default.nix
+++ pkgs/terraform-provider-arin/default.nix
@@ -3,9 +3,9 @@
 (terraform-providers.mkProvider {
   owner = "stepbrobd";
   repo = "terraform-provider-arin";
-  rev = "v2026.812.0";
-  hash = "sha256-f2ec6UkglRLTUJPgWYMTExAjXezK4LXRv1du42Mvh6k=";
-  vendorHash = "sha256-yjSn7MDYrBKI01BwRLfna2LA5HGuPSAfCq48O8fCLf4=";
+  rev = "v2026.815.0";
+  hash = "sha256-cvsAFKHXXLOHs5QeVBH3aPqScEQI+tFAUEzRMkqWNAw=";
+  vendorHash = "sha256-26uE3ef9C1pF3hvdPvm/UclewgMCQPhCjOyGtZWSFqE=";
   spdx = "MIT";
   provider-source-address = "registry.terraform.io/stepbrobd/arin";
 }).overrideAttrs { subPackages = [ "cmd/terraform-provider-arin" ]; }
