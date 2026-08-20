4e9efa141a871dece1c37aefcc72c680dd8a33f5
Yifei Sun <ysun@hey.com>
2026-08-20 13:16:06 +0200

    hosts/lagern: drop ena driver module

--- hosts/lagern/hardware.nix
+++ hosts/lagern/hardware.nix
@@ -3,6 +3,9 @@
 {
   imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
 
+  # TODO: remove after https://github.com/nixos/nixpkgs/pull/554254
+  boot.extraModulePackages = lib.mkForce [ ];
+
   # AWS 1:1 NAT
   # public IPv4 (16.62.113.214) is not on ens5 (172.31.34.75)
   # null the local address so strongswan resolves source via routing table
