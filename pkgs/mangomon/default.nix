8cca107949bc16d0dd9854fd343e1d21f0443014
Yifei Sun <ysun@hey.com>
2026-08-20 12:51:45 +0200

    pkgs/mangomon: init at 2026.820.0

---
+++ pkgs/mangomon/default.nix
@@ -0,0 +1,53 @@
+{ lib
+, buildGoModule
+, fetchFromGitHub
+, makeWrapper
+, wl-mirror
+, wlr-randr
+, versionCheckHook
+, nix-update-script
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "mangomon";
+  version = "2026.820.0";
+
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "stepbrobd";
+    repo = "mangomon";
+    tag = finalAttrs.version;
+    hash = "sha256-YX3CjHvrzBWIBIysmnCFRiBs1FEajXLjVAdDmyRLZJI=";
+  };
+
+  vendorHash = "sha256-eSKuDWtzRxwrRvBKA6z85P/+Lqf7djgqMVu3xv7ttDM=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.Version=${finalAttrs.version}"
+  ];
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  postFixup = ''
+    wrapProgram $out/bin/mangomon --prefix PATH : "${lib.makeBinPath [ wl-mirror wlr-randr ]}"
+  '';
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "-version";
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "TUI monitor configuration tool for mango wm with visual layout, drag-and-drop, and profile management";
+    homepage = "https://github.com/stepbrobd/mangomon";
+    changelog = "https://github.com/stepbrobd/mangomon/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ stepbrobd ];
+    mainProgram = "mangomon";
+    platforms = lib.platforms.linux;
+  };
+})
