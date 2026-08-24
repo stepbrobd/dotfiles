dda1f399baea3999f9a0f94427536d800f14d9ee
Yifei Sun <ysun@hey.com>
2026-08-24 21:16:28 +0200

    pkgs/caddy: use forked certmagic-s3 plugin

--- pkgs/caddy/default.nix
+++ pkgs/caddy/default.nix
@@ -3,12 +3,12 @@
 # pull caddy from `inputs` directly to prevent infinite recursion
 # as `caddy.withPlugins` is implemented with override
 inputs.nixpkgs.legacyPackages.${stdenv.hostPlatform.system}.caddy.withPlugins {
-  hash = "sha256-LabMY1rWe6Q2Otgq44vjr3U3RG1SxEwDfkg0z7o7LrU=";
+  hash = "sha256-Olvaar6yOtFGxpb/7rWAukkGxzUHcZuxGyoblmh+Y7w=";
   plugins = [
     "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
     "github.com/caddy-dns/cloudflare@v0.2.4"
     "github.com/relvacode/caddy-oidc@v0.3.1"
-    "github.com/ss098/certmagic-s3@v0.0.0-20250922022452-8af482af5f39"
+    "github.com/stepbrobd/certmagic-s3@v0.0.0-20260824190408-591008b07aa5"
     "github.com/ueffel/caddy-brotli@v1.6.0"
   ];
 }
