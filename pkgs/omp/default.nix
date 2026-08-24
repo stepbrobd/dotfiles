75527b1c0ffbc2c3c9bf58adc9787d962b20b562
Yifei Sun <ysun@hey.com>
2026-08-24 13:28:34 +0200

    pkgs/omp: darwin allow local networking

--- pkgs/omp/default.nix
+++ pkgs/omp/default.nix
@@ -1,1 +1,7 @@
-{ inputs, stdenv }: inputs.llm.packages.${stdenv.hostPlatform.system}.omp
+{ inputs, lib, stdenv }:
+
+inputs.llm.packages.${stdenv.hostPlatform.system}.omp.overrideAttrs (
+  lib.optionalAttrs stdenv.hostPlatform.isDarwin {
+    __darwinAllowLocalNetworking = true;
+  }
+)
