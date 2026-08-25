8d179e9085ffbe50751aa0f0642cd66632e71270
Yifei Sun <ysun@hey.com>
2026-08-25 20:38:51 +0200

    modules/home/media: set miruro config

--- modules/home/ysun/media/default.nix
+++ modules/home/ysun/media/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   programs.mpv = {
@@ -13,4 +13,12 @@
     miruro
     yt-dlp
   ];
+
+  xdg.configFile."miruro/config.toml".source = (pkgs.formats.toml { }).generate "miruro.toml" {
+    lang = "en";
+    dub = false;
+    quality = "best";
+    provider = "pewe:hard";
+    download = "${config.home.homeDirectory}/Videos";
+  };
 }
