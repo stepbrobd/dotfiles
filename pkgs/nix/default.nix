487c2d27e9a3899b9f27ebf208716f5490186d53
StepBroBD <ysun@hey.com>
2026-08-26 08:48:43 +0200

    pkgs/nix: drop merged patch

--- pkgs/nix/default.nix
+++ pkgs/nix/default.nix
@@ -1,8 +1,1 @@
-{ pkgsPrev, fetchpatch2 }:
-
-pkgsPrev.nixVersions.git.appendPatches [
-  (fetchpatch2 {
-    url = "https://github.com/stepbrobd/nix/commit/49baeb9b817e15305915f02aed876fefb440b26d.patch";
-    hash = "sha256-qNFD2BajP+JGAiU3rRbNDJwjezrpzWNaRAoYsgqH/lk=";
-  })
-]
+{ pkgsPrev  }: pkgsPrev.nixVersions.git
