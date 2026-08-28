a024085722731c6c261f3019a03112dfc20ff1ba
Yifei Sun <ysun@hey.com>
2026-08-28 13:20:26 +0200

    pkgs/niks3{,-server}: fix vendor hash

--- pkgs/niks3/default.nix
+++ pkgs/niks3/default.nix
@@ -2,5 +2,5 @@
 
 inputs.niks3.packages.${stdenv.hostPlatform.system}.niks3.overrideAttrs (prev: {
   src = inputs.niks3.outPath;
-  vendorHash = "sha256-pwutQpIbvNesoHwpEBKYWnovwMvrTWvyAz/7QqSMrGI=";
+  vendorHash = "sha256-lql+r9+hy7XX9/aSezwweKSU/MphxvIkaT0gHf59fsc=";
 })
