0822a9a91ad3b38cb9e6b3a05566ce64488fcd25
Yifei Sun <ysun@hey.com>
2026-08-29 19:28:44 +0200

    pkgs/niks3: drop unused args

--- pkgs/niks3/default.nix
+++ pkgs/niks3/default.nix
@@ -1,6 +1,6 @@
 { inputs, stdenv }:
 
-inputs.niks3.packages.${stdenv.hostPlatform.system}.niks3.overrideAttrs (prev: {
+inputs.niks3.packages.${stdenv.hostPlatform.system}.niks3.overrideAttrs {
   src = inputs.niks3.outPath;
   vendorHash = "sha256-lql+r9+hy7XX9/aSezwweKSU/MphxvIkaT0gHf59fsc=";
-})
+}
