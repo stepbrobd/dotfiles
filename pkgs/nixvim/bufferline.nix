f2d8475e74cfcfb6517747386c2adf50b31aed85
Yifei Sun <ysun@hey.com>
2026-08-30 23:11:52 +0200

    pkgs/nixvim: map buffer actions

--- pkgs/nixvim/bufferline.nix
+++ pkgs/nixvim/bufferline.nix
@@ -16,4 +16,19 @@
       ];
     };
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>bn";
+      action = "<cmd>BufferLineCycleNext<cr>";
+      options = { silent = true; desc = "Next buffer"; };
+    }
+    {
+      mode = "n";
+      key = "<leader>bp";
+      action = "<cmd>BufferLineCyclePrev<cr>";
+      options = { silent = true; desc = "Previous buffer"; };
+    }
+  ];
 }
