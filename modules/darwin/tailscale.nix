ac27c74229058dc892062614cbbf17709d4ec93e
Yifei Sun <ysun@hey.com>
2026-08-30 22:10:44 +0200

    modules/darwin/tailscale: fix guard

--- modules/darwin/tailscale.nix
+++ modules/darwin/tailscale.nix
@@ -3,16 +3,21 @@
 { config, ... }:
 
 let
-  inherit (lib) attrValues elem mkForce mkIf mkMerge;
+  inherit (lib) any attrValues elem mkForce mkIf mkMerge;
 in
 {
   config = mkMerge [
     { services.tailscale.overrideLocalDns = true; }
 
-    (mkIf (elem 1475387142 (attrValues config.homebrew.masApps)) {
-      # Tailscale: `mas search 1475387142`
-      services.tailscale.enable = mkForce false; # use tailscale from App Store
-      environment.shellAliases.tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
-    })
+    (mkIf
+      (
+        elem 1475387142 (attrValues config.homebrew.masApps)
+        ||
+        any (cask: elem cask.name [ "tailscale" "tailscale-app" ]) config.homebrew.casks
+      )
+      {
+        services.tailscale.enable = mkForce false; # use tailscale from homebrew
+        environment.shellAliases.tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
+      })
   ];
 }
