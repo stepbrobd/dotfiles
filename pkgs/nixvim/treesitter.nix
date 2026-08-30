d244a69f89d838d6e7daa856dac155db36b986ef
Yifei Sun <ysun@hey.com>
2026-08-30 15:55:47 +0200

    pkgs/nixvim: fix highlighting

--- pkgs/nixvim/treesitter.nix
+++ pkgs/nixvim/treesitter.nix
@@ -4,6 +4,7 @@
   plugins = {
     treesitter.enable = true;
     treesitter.folding.enable = true;
+    treesitter.highlight.enable = true;
     # treesitter-context.enable = true;
     # treesitter-refactor.enable = true;
     # treesitter-textobjects.enable = true;
