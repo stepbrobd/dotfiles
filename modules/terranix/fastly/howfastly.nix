abd4cea14e859b91f91a69bc07b5eb554138a71b
Yifei Sun <ysun@hey.com>
2026-09-01 13:23:41 +0200

    modules/terranix/fastly: link secret store and backend

--- modules/terranix/fastly/howfastly.nix
+++ modules/terranix/fastly/howfastly.nix
@@ -1,8 +1,15 @@
-{ ... }:
+{ lib, ... }:
 
+let
+  inherit (lib.terranix) tfRef;
+in
 {
+  resource.fastly_secretstore.howfastly = {
+    name = "HowFastly";
+  };
+
   resource.fastly_service_compute.howfastly = {
-    name = "speed.edgecompute.app";
+    name = "HowFastly";
 
     activate = false;
 
@@ -10,6 +17,21 @@
       { name = "speed.edgecompute.app"; }
       { name = "howfastly.edgecompute.app"; }
     ];
+
+    backend = [{
+      name = "fastly";
+      address = "api.fastly.com";
+      port = 443;
+      use_ssl = true;
+      ssl_cert_hostname = "api.fastly.com";
+      ssl_sni_hostname = "api.fastly.com";
+      override_host = "api.fastly.com";
+    }];
+
+    resource_link = [{
+      name = "howfastly";
+      resource_id = tfRef "fastly_secretstore.howfastly.id";
+    }];
 
     lifecycle.ignore_changes = [ "package" ];
   };
