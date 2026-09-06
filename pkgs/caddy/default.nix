d378fd944008084a901ea184d3bb9d3d5c42539a
Yifei Sun <ysun@hey.com>
2026-09-06 14:37:57 +0200

    pkgs/caddy: bump sigsci

--- pkgs/caddy/default.nix
+++ pkgs/caddy/default.nix
@@ -3,11 +3,11 @@
 # pull caddy from `inputs` directly to prevent infinite recursion
 # as `caddy.withPlugins` is implemented with override
 inputs.nixpkgs.legacyPackages.${stdenv.hostPlatform.system}.caddy.withPlugins {
-  hash = "sha256-DO6KVbToawusNDUrHFDlfiU0yIZTpC+q1kpc6YY0LNk=";
+  hash = "sha256-La3cyj+yjWEx+BjfkGD94bjSa9bgOSCA3LA0iX+rEbU=";
   plugins = [
     "github.com/caddy-dns/cloudflare@v0.2.4"
     "github.com/relvacode/caddy-oidc@v0.4.2"
-    "github.com/stepbrobd/caddy-sigsci@v0.0.0-20260906094734-ac316d34e555"
+    "github.com/stepbrobd/caddy-sigsci@v0.0.0-20260906123041-6d48f11ee173"
     "github.com/stepbrobd/certmagic-s3@v0.0.0-20260906102855-4c0666564d10"
     "github.com/ueffel/caddy-brotli@v1.6.0"
   ];
