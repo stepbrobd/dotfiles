ba42235a37e2bcc74b51897ed994af9af453a565
Yifei Sun <ysun@hey.com>
2026-09-06 13:22:42 +0200

    pkgs/caddy: drop cf ip and add sigsci

--- pkgs/caddy/default.nix
+++ pkgs/caddy/default.nix
@@ -3,12 +3,12 @@
 # pull caddy from `inputs` directly to prevent infinite recursion
 # as `caddy.withPlugins` is implemented with override
 inputs.nixpkgs.legacyPackages.${stdenv.hostPlatform.system}.caddy.withPlugins {
-  hash = "sha256-kP3gpVdLrAico5DIkitGgc4fekwqZu09S7/by06mgko=";
+  hash = "sha256-DO6KVbToawusNDUrHFDlfiU0yIZTpC+q1kpc6YY0LNk=";
   plugins = [
-    "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
     "github.com/caddy-dns/cloudflare@v0.2.4"
-    "github.com/relvacode/caddy-oidc@v0.4.0"
-    "github.com/stepbrobd/certmagic-s3@v0.0.0-20260824195825-3df4eadafe7e"
+    "github.com/relvacode/caddy-oidc@v0.4.2"
+    "github.com/stepbrobd/caddy-sigsci@v0.0.0-20260906094734-ac316d34e555"
+    "github.com/stepbrobd/certmagic-s3@v0.0.0-20260906102855-4c0666564d10"
     "github.com/ueffel/caddy-brotli@v1.6.0"
   ];
 }
